f3513db1c4e47c11f1f04783966c798f8e312ca6
Johnny-batata <you@example.com>
2021-04-08 16:59:43 -0300

    exercício 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,21 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+// const lion = {id} = data.animals[0]
+// // console.log( lion)
+// // console.log(data.animals[0])
+const { animals } = data;
+
+function animalsByIds(...ids) {
   // seu código aqui
+  const param = ids; // coloquei o parametro dentro dessa const para ficar mais organizado
+
+  const animalsMapped = param.map((IdCode) => // criei uma nova váriavel e apliquei dentro dela um map com os parametros para que eu possa transformar em something brand new
+    animals.find((animal) => animal.id === IdCode)); // dentro do map apliquei um find que vai procurar na variavel animals o primeiro id que possui o mesmo valor que o id do param do meu map
+
+  return animalsMapped; // no final da função pedi para retornar a minha const
 }
+animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'ef3778eb-2844-4c7c-b66c-f432073e1c6b');
 
 function animalsOlderThan(animal, age) {
   // seu código aqui

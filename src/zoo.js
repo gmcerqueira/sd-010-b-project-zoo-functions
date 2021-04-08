6d02545c7cbc017ad8fed67b47ede1ba6485824b
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-04-08 17:10:54 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -36,7 +36,7 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  return id;
+  return data.employees.some((param) => param.managers.includes(id));
 }
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {

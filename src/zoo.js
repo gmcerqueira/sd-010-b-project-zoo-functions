cf76457475c8accfcd67f036a30d37a5e4849bb2
Huggo Parcelly <h.parcelly@gmail.com>
2021-04-08 19:32:43 -0300

    adiciona a funcao que verifica se o funcionario eh do cargo de gerencia (desafio 4)

--- src/zoo.js
+++ src/zoo.js
@@ -52,9 +52,14 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  // seu código aqui
+  // Verifica se uma pessoa colaboradora, a partir de seu id, ocupa cargo de gerência.
+  // Deve retornar um valor booleano. Testa se o id passado é de um gerente
+  // capturar o id do funcionario verificar se o cargo é gerencia(só responder a uma pessoa - 1 manager)
+  const employee = employees.find((employe) => employe.id === id); // infos funcionário
+  return employee.managers.length === 1;
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -97,7 +102,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

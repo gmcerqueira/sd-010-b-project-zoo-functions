8f5fa496c3b9b7c9abc88d96b819a5ef0fd95032
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-08 17:44:10 -0300

    feito tarefa 5

--- src/zoo.js
+++ src/zoo.js
@@ -37,11 +37,10 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
-/*
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
-
+/*
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
@@ -83,7 +82,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

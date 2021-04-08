4f8f86cf2be00a754ad666b8a0738015233d3711
Jessie <miss_sc_jessi@hotmail.com>
2021-04-08 17:49:14 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -37,9 +37,9 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return empregados.some((ids) => ids.managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -82,7 +82,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

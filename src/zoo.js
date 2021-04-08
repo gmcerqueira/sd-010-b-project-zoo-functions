9b149e6d2832a1b8a9f2d5d5f1eed6de173d2cfd
Fernando <fhomaia@gmail.com>
2021-04-08 17:21:29 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,10 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  // seu código aqui
+  return data.employees.some((employee) => employee.managers.find((idCode) => idCode === id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -86,7 +87,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

688c5968f2834ccd8938f0f991f5cc9557863cbc
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-04-08 14:46:13 -0300

    Ajustes de Lint

--- src/zoo.js
+++ src/zoo.js
@@ -9,30 +9,35 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
 
 // Faz a desestructuringObjects para uso nas funções
-const { animals, employees, prices, hours } = data;
+const { animals, employees, prices } = data;
 
 function animalsByIds(...args) {
-  // 1- Filter traz todos os animais e o some 
+  // 1- Filter traz todos os animais e o some
   // 2- Some utilizado junto com o filter retorna somente o animal que atender a condição
   // 3- ...args pode trazer um id ou vários ids, é o spread
-  return animals.filter((animal) => (args.some(checkId => animal.id === checkId)));
+  return animals.filter((animal) =>
+    args.some((checkId) => animal.id === checkId)
+  );
 }
 
 function animalsOlderThan(animal, age) {
   // 1- Find retorna o primeiro objeto que atenda a condição passada
   // 2- Dentro do objeto retornado tem outro array com mais objetos onde é verificado o value da key age de cada um e comparado com o parâmetro passado isso com a função every.
-  const resultado = animals.find((dataAnimal) => (dataAnimal.name === animal));
-  return resultado.residents.every((dataAge) => (dataAge.age >= age))
+  const resultado = animals.find((dataAnimal) => dataAnimal.name === animal);
+  return resultado.residents.every((dataAge) => dataAge.age >= age);
 }
 
 function employeeByName(employeeName) {
   // 1- É verificado se employeeName é vazio ou nulo, se for é retornado um objeto vazio.
   if (!employeeName) return {};
   // 2- Find retorna o primeiro objeto que corresponda a condição passada
-  return employees.find((employ) => (employeeName === employ.firstName) || (employeeName === employ.lastName));
+  return employees.find(
+    (employ) =>
+      employeeName === employ.firstName || employeeName === employ.lastName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -43,10 +48,16 @@ function createEmployee(personalInfo, associatedWith) {
 function isManager(id) {
   // 1- Some retorna verdadeiro ou falso através da condição informada
   // 2- Includes verifica dentro de um array ou objeto se contém o item informado
-  return employees.some((manager) => (manager.managers.includes(id)));
+  return employees.some((manager) => manager.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
   // 1- Alterado os parâmetros recebido managers = [], responsibleFor = [] para que se nada for informado seja inserido por padrão um array vazio a cada.
   // 2- Através dos parâmetros passados é criado um novo objeto.
   // 3- Ao dar um push no array employees o novo objeto é adicionado ao final do array
@@ -58,7 +69,8 @@ function animalCount(species) {
   // 1- É verificado se species é vazio ou nulo, se for é retornado um objeto vazio.
   if (!species) return {};
   // 2- A função find traz o objeto com o parâmetro informado, dentro deste objeto tem um segundo array de objetos com o nome residents e usando residents.length verifica o tamanho do array
-  return animals.find((speciesReturned) => (speciesReturned.name === species)).residents.length;
+  return animals.find((speciesReturned) => speciesReturned.name === species)
+    .residents.length;
 }
 
 function entryCalculator(entrants) {
@@ -69,7 +81,9 @@ function entryCalculator(entrants) {
   // 3- Feito um for onde percorre o objeto e o Object.keys retorna as keys, ou seja Adult, Child e Senior
   // 4- entrants[value] retorna o valor contido (qtdade de pessoas) nas posições Adult no primeiro loop, Child  no segundo loop e Senior no terceiro loop.
   // Em cada loop ele recupera o valor da entrada em prices passando a chave do objeto como referência (Adult, Child e Senior) e multiplica esse valor com a quantidade de pessoas obtidas na chave e guarda na variável totalValue para retornar no fim do for.
-  Object.keys(entrants).forEach(value => (totalValue += entrants[value] * prices[value]));
+  Object.keys(entrants).forEach(
+    (value) => (totalValue += entrants[value] * prices[value])
+  );
   return totalValue;
 }
 

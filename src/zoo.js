850a3910a76a8a99c42e8b56de9d918e5d99d09d
Malbathan Lima <malbathanlima@gmail.com>
2021-04-08 17:08:43 -0300

    isManager

--- src/zoo.js
+++ src/zoo.js
@@ -34,9 +34,10 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  // seu código aqui
+  return employees.some((employees) => (employees.managers.includes(id)));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -79,7 +80,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

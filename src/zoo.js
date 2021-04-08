da391952286bc64b1cc67dc893e1504911351c25
Arlen Freitas <arlenfreitas1@gmail.com>
2021-04-08 16:25:25 -0300

    isManager

--- src/zoo.js
+++ src/zoo.js
@@ -37,9 +37,9 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((employee) => employee.managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -82,7 +82,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

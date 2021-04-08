af92ced7aaf199a863993f3ab4f0fdda875d6404
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-04-08 15:37:25 -0300

    Requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -18,50 +18,63 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  const findAnimal = data.animals.find(({ name }) => name === animal);
+  return findAnimal.residents.every((elemento => elemento.age >= age))
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  return employeeName
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return personalInfo, associatedWith
 }
 
 function isManager(id) {
   // seu código aqui
+  return id
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  return id, firstName, lastName, managers, responsibleFor
 }
 
 function animalCount(species) {
   // seu código aqui
+  return species
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  return entrants
 }
 
 function animalMap(options) {
   // seu código aqui
+  return options
 }
 
 function schedule(dayName) {
   // seu código aqui
+  return dayName
 }
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  return id
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  return percentage
 }
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  return idOrName
 }
 
 module.exports = {

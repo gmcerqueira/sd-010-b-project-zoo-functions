d73c2f3fe638f4d12ac3e983d4430c39f97e3947
Camila Arruda <camilaam1802@gmail.com>
2021-04-08 16:11:38 -0300

    Resoluçao requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,71 +11,88 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-  //começando
+const { animals } = data;
+
+function animalsByIds(...ids) {
+  // Caso receba nenhum parâmetro, necessário retornar um array vazio
+  // Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
+  // Ao receber mais de um id, retorna um array com as espécies referentes aos ids
+  if (!ids) return [];
+  return animals.filter((animal) => ids.includes(animal.id));
 }
 
-function animalsOlderThan(animal, age) {
+/* function animalsOlderThan(animal, age) {
   // seu código aqui
+  return animal, age;
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  return employeeName;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return personalInfo, associatedWith;
 }
 
 function isManager(id) {
   // seu código aqui
+  return id;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  return id, firstName, lastName, managers, responsibleFor;
 }
 
 function animalCount(species) {
   // seu código aqui
+  return species;
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  return entrants;
 }
 
 function animalMap(options) {
   // seu código aqui
+  return options;
 }
 
 function schedule(dayName) {
   // seu código aqui
+  return dayName;
 }
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  return id;
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  return percentage;
 }
 
 function employeeCoverage(idOrName) {
   // seu código aqui
-}
+  return idOrName;
+} */
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  // entryCalculator,
+  // schedule,
+  // animalCount,
+  // animalMap,
   animalsByIds,
-  employeeByName,
+  /* employeeByName,
   employeeCoverage,
   addEmployee,
   isManager,
   animalsOlderThan,
   oldestFromFirstSpecies,
   increasePrices,
-  createEmployee,
+  createEmployee, */
 };

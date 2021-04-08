70104e62589b101d972c921ac285da5e3ec5545d
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-04-08 16:11:24 -0300

    2. IMPLEMENTE A FUNÇÃO animalsOlderThan

--- src/zoo.js
+++ src/zoo.js
@@ -24,11 +24,16 @@ function animalsByIds(...ids) {
 }
 
 // console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+/* console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46')); */
+
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  const animalName = data.animals.find((animalItem) => animalItem.name === animal);
+  return animalName.residents.every((resident) => resident.age > age);
 }
+
+// console.log(animalsOlderThan('otters', 7));
 
 function employeeByName(employeeName) {
   // seu código aqui

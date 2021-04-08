7905bc6f6474ba9f2280c297e873246999294814
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-08 18:49:10 -0300

    add exercicios 5

--- src/zoo.js
+++ src/zoo.js
@@ -39,29 +39,17 @@ function createEmployee(personalInfo, associatedWith) {
     ...personalInfo, ...associatedWith,
   };
 }
-
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992',
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-  ],
-};
-
-console.log(createEmployee(personalInfo, associatedWith));
-
 function isManager(id) {
-  // seu código aqui
+  return data.employees
+    .some((employe) => employe.managers.some((manager) => id === manager));
 }
+
+/* function isManager(id) {
+  return data.employees
+    .some((employe, index) => id === employe.managers[index]);
+} */
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui

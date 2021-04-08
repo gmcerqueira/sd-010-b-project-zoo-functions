c1c9227557ae6fc60c5f31606cad1668ba32b188
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-04-08 15:37:37 -0300

    Adiciona a funçao employeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,9 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {

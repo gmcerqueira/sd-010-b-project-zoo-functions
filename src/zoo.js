ea2b587130df403d1a0dc164e0f228d2d285eb10
Matheus Carretta <matheuscarretta@gmail.com>
2021-04-08 16:40:55 -0300

    Iniciando o projeto

--- src/zoo.js
+++ src/zoo.js
@@ -13,38 +13,47 @@ const data = require('./data');
 
 function animalsByIds(ids) {
   // seu código aqui
+  return ids;
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return animal;
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  return employeeName;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return personalInfo;
 }
 
 function isManager(id) {
   // seu código aqui
+  return id;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  return id;
 }
 
 function animalCount(species) {
   // seu código aqui
+  return species;
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  return entrants;
 }
 
 function animalMap(options) {
   // seu código aqui
+  return options;
 }
 
 function schedule(dayName) {
@@ -53,14 +62,17 @@ function schedule(dayName) {
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  return id;
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  return percentage;
 }
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  return idOrName;
 }
 
 module.exports = {

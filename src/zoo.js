d149ebe3b676ab3de00aa7ee8adb614593e63b7a
Camila <camilafabiabh9@gmail.com>
2021-04-08 16:42:22 -0300

    adicionando function

--- src/zoo.js
+++ src/zoo.js
@@ -10,16 +10,27 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const {animals} = data;
+const {animals, employees} = data;
 function animalsByIds(...ids) {
-  return animals.filter((animal) => ids.some((verificaId) => animal.id === verificaId));
-  
+  return animals.filter((animal) => ids.some((verificaId) => animal.id === verificaId));  
 }
 // console.log(animalsByIds())
-/*
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const retorno = animals.find((animalD) => animalD.name === animal);
+  return retorno.residents.every((ageD) => ageD.age >= age);
 }
+/*Esta função, a partir do nome de uma espécie e uma idade mínima, 
+verifica se todos os animais daquela espécie possuem a idade mínima especificada
+
+  **Observações técnicas**
+
+  - Deve retornar um valor booleano 
+
+  **O que será avaliado**
+
+  - Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta
+ espécie possuem a idade mínima especificada
+
 
 function employeeByName(employeeName) {
   // seu código aqui

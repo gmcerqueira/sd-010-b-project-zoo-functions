07e00bc481594ea292e6c775c9101d30e8388546
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-04-08 16:33:01 -0300

    Implementa a funçao entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -64,8 +64,10 @@ function animalCount(species) {
   return animal.residents.length;
 }
 
-function entryCalculator(entrants) {
-  // seu código aqui
+function entryCalculator(entrants = {}) {
+  if (entrants === {}) return 0;
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  return ((Adult * 49.99) + (Child * 20.99) + (Senior * 24.99));
 }
 
 function animalMap(options) {

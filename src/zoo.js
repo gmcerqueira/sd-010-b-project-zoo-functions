2616d5ddfb3d2b7991456f6377c36f2be6a4463e
EderPaiva <edergomesdepaiva12@gmail.com>
2021-04-08 20:03:18 -0300

    1 e 2

--- src/zoo.js
+++ src/zoo.js
@@ -11,12 +11,41 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+
+  if (ids.length === 0) {
+    return [];
+  }
+  const animalReduce = [...ids];
+  const animalSeach = animalReduce.reduce((animalIdAcc, id) => {
+    // onde estão os animais?
+    const animals = data.animals;
+    // pegue o id e vá de encontro ao animal.
+    const locator = animals.find((animal) => animal.id === id);
+    // armazene o animal
+    return animalIdAcc.concat(locator);
+  }, []);
+  // retorne todos animais.
+  return animalSeach;
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  //onde está o animal?
+  const {animals} = data;
+  // qual é este animal?
+  const animalSeach = animals.find(({
+    name
+  }) => name === animal);
+  // array com todos da mesma especie;
+  const sameSpecies = animalSeach.residents.reduce((espAcc, espAnimal) => {
+    if (espAnimal.age < age) {
+      return espAcc = false;
+    }
+    // ele sempre retornará o ultimo valor.
+    return espAcc;
+  }, true);
+  return sameSpecies;
 }
 
 function employeeByName(employeeName) {

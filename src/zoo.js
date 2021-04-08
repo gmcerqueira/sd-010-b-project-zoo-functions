03681f4c801f79d4ccc63a74ad59c527ab01fcff
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-04-08 15:44:54 -0300

    lint errors

--- src/zoo.js
+++ src/zoo.js
@@ -11,71 +11,72 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
+console.log(data.animals);
+
 function animalsByIds(ids) {
-  // seu código aqui
-  
+  return ids;
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// function animalsOlderThan(animal, age) {
 
-function employeeByName(employeeName) {
-  // seu código aqui
-}
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function employeeByName(employeeName) {
 
-function isManager(id) {
-  // seu código aqui
-}
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
 
-function animalCount(species) {
-  // seu código aqui
-}
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function isManager(id) {
 
-function animalMap(options) {
-  // seu código aqui
-}
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function animalCount(species) {
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// }
+
+// function entryCalculator(entrants) {
+
+// }
+
+// function animalMap(options) {
+
+// }
+
+// function schedule(dayName) {
+
+// }
+
+// function oldestFromFirstSpecies(id) {
+
+// }
+
+// function increasePrices(percentage) {
+
+// }
+
+// function employeeCoverage(idOrName) {
+
+// }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+//   entryCalculator,
+//   schedule,
+//   animalCount,
+//   animalMap,
   animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+//   employeeByName,
+//   employeeCoverage,
+//   addEmployee,
+//   isManager,
+//   animalsOlderThan,
+//   oldestFromFirstSpecies,
+//   increasePrices,
+//   createEmployee,
 };

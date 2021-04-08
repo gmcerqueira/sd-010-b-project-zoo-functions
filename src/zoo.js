0b078c5f6ce7707f1423ad8e48402a00f8223057
Matheus Carretta <matheuscarretta@gmail.com>
2021-04-08 17:46:00 -0300

    Requisito 1 resolvido

--- src/zoo.js
+++ src/zoo.js
@@ -11,82 +11,83 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-  return ids;
+const { animals } = data;
+
+function animalsByIds(...ids) {
+  return animals.filter((animal) => ids.some((id) => animal.id === id));
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-  return animal;
-}
+// function animalsOlderThan(animal, age) {
+//   // seu código aqui
+//   return animal;
+// }
 
-function employeeByName(employeeName) {
-  // seu código aqui
-  return employeeName;
-}
+// function employeeByName(employeeName) {
+//   // seu código aqui
+//   return employeeName;
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-  return personalInfo;
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+//   return personalInfo;
+// }
 
-function isManager(id) {
-  // seu código aqui
-  return id;
-}
+// function isManager(id) {
+//   // seu código aqui
+//   return id;
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-  return id;
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+//   return id;
+// }
 
-function animalCount(species) {
-  // seu código aqui
-  return species;
-}
+// function animalCount(species) {
+//   // seu código aqui
+//   return species;
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-  return entrants;
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+//   return entrants;
+// }
 
-function animalMap(options) {
-  // seu código aqui
-  return options;
-}
+// function animalMap(options) {
+//   // seu código aqui
+//   return options;
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-  return id;
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+//   return id;
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-  return percentage;
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+//   return percentage;
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-  return idOrName;
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+//   return idOrName;
+// }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  // entryCalculator,
+  // schedule,
+  // animalCount,
+  // animalMap,
   animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  // employeeByName,
+  // employeeCoverage,
+  // addEmployee,
+  // isManager,
+  // animalsOlderThan,
+  // oldestFromFirstSpecies,
+  // increasePrices,
+  // createEmployee,
 };

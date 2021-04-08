e8f8e31a722e390d930d7ae5ce47dececa436058
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-04-08 17:49:59 -0300

    exercicio 5

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,10 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmployee;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const checkid = data.employees.some((element) => element.managers.includes(id));
+  return checkid;
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -86,7 +87,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

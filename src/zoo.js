4183340bd9e8e7f2d8d782058611e78d14fe384d
Malbathan Lima <malbathanlima@gmail.com>
2021-04-08 18:41:53 -0300

    isMAnager fix

--- src/zoo.js
+++ src/zoo.js
@@ -34,10 +34,10 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-//   return employees.some((isManager) => (isManager.managers.includes(id)));
-// }
+function isManager(id) {
+  // seu código aqui
+  return employees.some((employee) => (employee.managers.includes(id)));
+}
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
@@ -94,7 +94,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

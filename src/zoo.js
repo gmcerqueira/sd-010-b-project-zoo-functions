60f97d7f6a36925b6b0be8bfc86e5f12211efb08
Misael Bueno <misa.hbueno@gmail.com>
2021-04-08 20:21:51 -0300

    requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -47,11 +47,30 @@ function employeeByName(employeeName) {
   return name[0];
 }
 
-// console.log(employeeByName('Wishart'))
+function createEmployee(personalInfo, associatedWith) {
+  const newEmployee = { ...personalInfo, ...associatedWith };
+  return newEmployee;
+}
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+// const personalInfo = {
+//   id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+//   firstName: 'John',
+//   lastName: 'Doe',
+// };
+
+// const associatedWith = {
+//   managers: [
+//     'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//     '9e7d4524-363c-416a-8759-8aa7e50c0992'
+//   ],
+//   responsibleFor: [
+//     '0938aa23-f153-4937-9f88-4858b24d6bce',
+//     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+//   ]
+// };
+
+// console.log(createEmployee(personalInfo, associatedWith));
 
 // function isManager(id) {
 //   // seu código aqui
@@ -102,5 +121,5 @@ module.exports = {
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };

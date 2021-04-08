da1b2bc8460c1caeebadee9f85900c8550b73d73
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-04-08 15:44:06 -0300

    1. IMPLEMENTE A FUNÇÃO animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,10 +11,21 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  let messege = null;
+  if (ids === undefined) {
+    messege = [];
+  } else {
+    messege = [];
+    ids.forEach((id) => messege.push(...data.animals.filter((animal) => animal.id === id)));
+  }
+  return messege;
 }
 
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 function animalsOlderThan(animal, age) {
   // seu código aqui
 }

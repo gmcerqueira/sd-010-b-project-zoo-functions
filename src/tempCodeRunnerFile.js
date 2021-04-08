3fe8212c2b85433fb1912b3d5107873aea3cd435
Antenor <antenorzapata@gmail.com>
2021-04-08 20:01:12 -0300

    ex13-lint correçao

--- src/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-  // if (idOrName.length > 10) {
-  //   const person = employees.filter((elem) => elem.id === idOrName);
-  //   const nome = `${person[0].firstName} ${person[0].lastName}`;
-  //   obj[nome] = person[0].responsibleFor.map((i) => animals.find((j) => j.id === i).name);
-  // } else {
-  //   const p = employees.filter((elem) => elem.firstName === idOrName || elem.lastName === idOrName);
-  //   const nome = `${p[0].firstName} ${p[0].lastName}`;
-  //   obj[nome] = p[0].responsibleFor.map((i) => animals.find((j) => j.id === i).name);
-  // }

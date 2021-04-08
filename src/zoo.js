9f9613cbf4f5d7720a07653ca12a609a814c9e32
esionascimento <esio.392@protonmail.com>
2021-04-08 16:52:03 -0300

    concluido parte 1

--- src/zoo.js
+++ src/zoo.js
@@ -12,70 +12,74 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  if (ids === 'undefined') {
+    return [];
+  }
+  const findAnimal = data.animals.filter((animal) => ids.find((id) => animal.id === id));
+  return findAnimal;
 }
 
-function animalsOlderThan(animal, age) {
+/* function animalsOlderThan(animal, age) {
   // seu código aqui
-}
+} */
 
-function employeeByName(employeeName) {
+/* function employeeByName(employeeName) {
   // seu código aqui
-}
+} */
 
-function createEmployee(personalInfo, associatedWith) {
+/* function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
-}
+} */
 
-function isManager(id) {
+/* function isManager(id) {
   // seu código aqui
-}
+} */
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+/* function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
-}
+} */
 
-function animalCount(species) {
+/* function animalCount(species) {
   // seu código aqui
-}
+} */
 
-function entryCalculator(entrants) {
+/* function entryCalculator(entrants) {
   // seu código aqui
-}
+} */
 
-function animalMap(options) {
+/* function animalMap(options) {
   // seu código aqui
-}
+} */
 
-function schedule(dayName) {
+/* function schedule(dayName) {
   // seu código aqui
-}
+} */
 
-function oldestFromFirstSpecies(id) {
+/* function oldestFromFirstSpecies(id) {
   // seu código aqui
-}
+} */
 
-function increasePrices(percentage) {
+/* function increasePrices(percentage) {
   // seu código aqui
-}
+} */
 
-function employeeCoverage(idOrName) {
+/* function employeeCoverage(idOrName) {
   // seu código aqui
-}
+} */
 
 module.exports = {
-  entryCalculator,
+  /* entryCalculator,
   schedule,
   animalCount,
-  animalMap,
+  animalMap, */
   animalsByIds,
-  employeeByName,
+  /* employeeByName,
   employeeCoverage,
   addEmployee,
   isManager,
   animalsOlderThan,
   oldestFromFirstSpecies,
   increasePrices,
-  createEmployee,
+  createEmployee, */
 };

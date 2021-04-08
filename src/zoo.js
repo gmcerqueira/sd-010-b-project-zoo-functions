5980c20ec4aced314faaca62ce5995560182d331
tonpasso <wellpasso@gmail.com>
2021-04-08 16:38:50 -0300

    Lint

--- src/zoo.js
+++ src/zoo.js
@@ -16,50 +16,62 @@ function animalsByIds(...ids) {
 }
 
 function animalsOlderThan(animal, age) {
+  return animal + age;
   // seu código aqui
 }
 
 function employeeByName(employeeName) {
+  return employeeName;
   // seu código aqui
 }
 
 function createEmployee(personalInfo, associatedWith) {
+  return personalInfo + associatedWith;
   // seu código aqui
 }
 
 function isManager(id) {
+  return id;
   // seu código aqui
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  return id + firstName + lastName + managers + responsibleFor;
   // seu código aqui
 }
 
 function animalCount(species) {
+  return species;
   // seu código aqui
 }
 
 function entryCalculator(entrants) {
+  return entrants;
   // seu código aqui
 }
 
 function animalMap(options) {
+  return options;
   // seu código aqui
 }
 
 function schedule(dayName) {
+  return dayName;
   // seu código aqui
 }
 
 function oldestFromFirstSpecies(id) {
+  return id;
   // seu código aqui
 }
 
 function increasePrices(percentage) {
+  return percentage;
   // seu código aqui
 }
 
 function employeeCoverage(idOrName) {
+  return idOrName;
   // seu código aqui
 }
 

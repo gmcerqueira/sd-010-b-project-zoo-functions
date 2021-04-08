92ee5351e7a80474986adcb3c9441d6e7a444403
Mariana Mohr <marianamohr@gmail.com>
2021-04-08 16:51:01 -0300

    requisito 1- feito - tirado ocnsole.log

--- src/zoo.js
+++ src/zoo.js
@@ -18,7 +18,6 @@ function animalsByIds(...ids) {
   return data.animals.filter((animal) => ids.some((id) => animal.id === id));
 }
 
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 // function animalsOlderThan(animal, age) {
 //   // seu código aqui
 // }

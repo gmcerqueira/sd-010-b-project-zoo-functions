20416c716e4f22e8a2e31c8b04f203b155a51ac6
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-04-08 16:24:58 -0300

    req 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,15 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui 
+function animalsByIds(...ids) {
+  // let idsArray;
+  // idsArray = ids;
+  let objArray = [];
+  objArray.push(data.animals.filter((animal) => ids.some((id) => animal.id === id)));
+  
+  return objArray[0];
 }
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui

5f5a57a1c44c663c0914c1a807957827b9265dc1
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-04-09 12:13:32 -0300

    animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+const { animals } = data;
+
+function animalsByIds(...ids) {
   // seu código aqui
+  return animals.filter((animalsId, i) => animalsId.id === ids[i]);
 }
 
 function animalsOlderThan(animal, age) {

ac81100165a59b7514932bfccab222f7b8143c0b
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-04-09 20:13:55 -0300

    Resolvendo requisito 5 do projeto

--- src/zoo.js
+++ src/zoo.js
@@ -51,9 +51,9 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmploy;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some(({ managers }) => managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -96,7 +96,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

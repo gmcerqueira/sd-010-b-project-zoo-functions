fc63c957866f8924cfa29763d84bd51595a3da08
Antenor <antenorzapata@gmail.com>
2021-04-09 07:22:47 -0300

    correçao ex13

--- src/zoo.js
+++ src/zoo.js
@@ -167,13 +167,13 @@ function increasePrices(percentage) {
 const makeName = (idOrName) => {
   const obj = {};
   if (idOrName.length > 10) {
-    const person = employees.filter((elem) => elem.id === idOrName);
-    const nome = `${person[0].firstName} ${person[0].lastName}`;
-    obj[nome] = person[0].responsibleFor.map((i) => animals.find((j) => j.id === i).name);
+    const person = employees.find((elem) => elem.id === idOrName);
+    const nome = `${person.firstName} ${person.lastName}`;
+    obj[nome] = person.responsibleFor.map((i) => animals.find((j) => j.id === i).name);
   } else {
-    const p = employees.filter((elem) => elem.firstName === idOrName || elem.lastName === idOrName);
-    const nome = `${p[0].firstName} ${p[0].lastName}`;
-    obj[nome] = p[0].responsibleFor.map((i) => animals.find((j) => j.id === i).name);
+    const p = employees.find((elem) => elem.firstName === idOrName || elem.lastName === idOrName);
+    const nome = `${p.firstName} ${p.lastName}`;
+    obj[nome] = p.responsibleFor.map((i) => animals.find((j) => j.id === i).name);
   }
   return obj;
 };

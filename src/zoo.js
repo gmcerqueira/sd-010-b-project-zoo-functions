2129eb7cdaf9cff31190cbf17b52deee1aec7a72
Anderson <trkz.anderson@gmail.com>
2021-04-09 17:31:40 -0300

    [Requisito 01] - animalsById

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,14 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const { animals } = data;
+
+function animalsByIds(...ids) {
+  return animals.filter((animal) => {
+    return ids.find((id) => {
+      return animal.id === id
+    });
+  })
 }
 
 function animalsOlderThan(animal, age) {

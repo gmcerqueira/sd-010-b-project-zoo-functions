73f4c63eb6221da73a3049896c422d3a0e9cdbcb
Adão Benites <adaobjr@gmail.com>
2021-04-09 17:31:55 -0400

    Ajuste de Formatação do arquivo Zoo

--- src/zoo.js
+++ src/zoo.js
@@ -13,8 +13,8 @@ const { animals } = require('./data');
 const { employees } = require('./data');
 const { prices } = require('./data');
 const { hours } = require('./data');
-// const data = require('./data');
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 1. IMPLEMENTE A FUNÇÃO animalsByIds
 // Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies
 // referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
@@ -24,6 +24,7 @@ function animalsByIds(...ids) {
 }
 // console.log(animalsByIds());
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 2. IMPLEMENTE A FUNÇÃO animalsOlderThan
 // Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela
 // espécie possuem a idade mínima especificada
@@ -34,6 +35,7 @@ function animalsOlderThan(animal, age) {
 }
 // console.log(animalsOlderThan('otters', 7));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 3. IMPLEMENTE A FUNÇÃO employeeByName
 // Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
 
@@ -48,6 +50,7 @@ function employeeByName(employeeName) {
 }
 // console.log(employeeByName('Emery'));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 4. IMPLEMENTE A FUNÇÃO createEmployee
 // A função, a partir de informações recebidas nos parâmetros, é capaz de criar um objeto equivalente
 // ao de uma pessoa colaboradora, retornando-o
@@ -56,6 +59,7 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 5. IMPLEMENTE A FUNÇÃO isManager
 // Verifica se uma pessoa colaboradora, a partir de seu id, ocupa cargo de gerência.
 
@@ -64,6 +68,7 @@ function isManager(id) {
 }
 // console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 6. IMPLEMENTE A FUNÇÃO addEmployee
 // A função irá adicionar uma nova pessoa colaboradora ao array employees, presente no arquivo data.js
 
@@ -72,6 +77,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 // console.log(addEmployee('122', 'Ad', 'Jr', ['1234', '444'], ['1255', '455']));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 7. IMPLEMENTE A FUNÇÃO animalCount
 // Esta função é responsável por contabilizar a quantidade de animais.
 
@@ -88,6 +94,7 @@ function animalCount(species) {
 // console.log(animalCount('penguins'));
 // console.log(animalCount());
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 8. IMPLEMENTE A FUNÇÃO entryCalculator
 // A partir da quantidade de visitantes e a faixa etária de cada um, esta função é responsável
 // por retornar o preço total a ser cobrado
@@ -97,6 +104,11 @@ function entryCalculator(entrants) {
     .reduce((acc, currVal) => acc + (entrants[currVal] * prices[currVal]), 0);
 }
 // console.log(entryCalculator({ Adult: 1, Senior: 2 }));
+
+// ----------------------------------------------------------------------------------------------------------------------
+// 9. IMPLEMENTE A FUNÇÃO animalMap
+// A função é responsável pelo mapeamento geográfico das espécies e seus animais, podendo ainda
+// filtrá-los por ordem alfabética e gênero, por exemplo
 
 function animalLocations() {
   return animals.reduce((acc, animal) => {
@@ -150,6 +162,7 @@ function animalMap(options = '') {
 // const options = { includeNames: true, sex: 'female', sorted: true };
 // console.log(animalMap(options));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 10. IMPLEMENTE A FUNÇÃO schedule
 // A função é responsável por disponibilizar as informações de horário para uma consulta,
 // que pode querer ter acesso a todo o cronograma da semana ou apenas o cronograma de um dia específico
@@ -170,6 +183,7 @@ function schedule(dayName) {
 }
 // console.log(schedule('Saturday'));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 11. IMPLEMENTE A FUNÇÃO oldestFromFirstSpecies
 // A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa
 // colaboradora do parâmetro
@@ -182,6 +196,7 @@ function oldestFromFirstSpecies(id) {
 }
 // console.log(oldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 12. IMPLEMENTE A FUNÇÃO increasePrices
 // A função é responsável por aumentar o preço das visitas, com base no valor de aumento recebido
 // no parâmetro, em porcentagem
@@ -195,6 +210,7 @@ function increasePrices(percentage) {
 }
 // console.log(increasePrices(50));
 
+// ----------------------------------------------------------------------------------------------------------------------
 // 13. IMPLEMENTE A FUNÇÃO employeeCoverage
 // A função é responsável por consultar as espécies pela qual a pessoa colaborada, recebida no
 // parâmetro através de seu id, firstName ou lastName, é responsável

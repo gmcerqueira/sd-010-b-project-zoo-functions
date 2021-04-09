0785b84460d25c3998e3343ed9f003cec1fe40d1
Valeriandreoni <valeriandreoni@gmail.com>
2021-04-09 19:27:30 -0300

    requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -17,51 +17,51 @@ function animalsByIds(...ids) {
   return zoo.filter((animal) => ids.some((animalId) => animal.id === animalId));
 }
 
-/* function animalsOlderThan(animal, age) {
-  // seu código aqui
+function animalsOlderThan(animal, age) {
+  const procurando = zoo.find((bicho) => bicho.name === animal);
+  return procurando.residents.every((residentes) => residentes.age > age);
 }
-
-function employeeByName(employeeName) {
+/* function employeeByName(employeeName) {
   // seu código aqui
-}
+} */
 
-function createEmployee(personalInfo, associatedWith) {
+/* function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
-}
+} */
 
-function isManager(id) {
+/* function isManager(id) {
   // seu código aqui
-}
+} */
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+/* function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
-}
+} */
 
-function animalCount(species) {
+/* function animalCount(species) {
   // seu código aqui
-}
+} */
 
-function entryCalculator(entrants) {
+/* function entryCalculator(entrants) {
   // seu código aqui
-}
+} */
 
-function animalMap(options) {
+/* function animalMap(options) {
   // seu código aqui
-}
+} */
 
-function schedule(dayName) {
+/* function schedule(dayName) {
   // seu código aqui
-}
+} */
 
-function oldestFromFirstSpecies(id) {
+/* function oldestFromFirstSpecies(id) {
   // seu código aqui
-}
+} */
 
-function increasePrices(percentage) {
+/* function increasePrices(percentage) {
   // seu código aqui
-}
+} */
 
-function employeeCoverage(idOrName) {
+/* function employeeCoverage(idOrName) {
   // seu código aqui
 } */
 
@@ -75,7 +75,7 @@ module.exports = {
   // employeeCoverage,
   // addEmployee,
   // isManager,
-  // animalsOlderThan,
+  animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
   // createEmployee,

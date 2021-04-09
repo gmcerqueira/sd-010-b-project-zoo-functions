f79dc6a9e80e69da355ba3da4e4b84c49336bc73
Jessie <miss_sc_jessi@hotmail.com>
2021-04-09 16:32:12 -0300

    requisito 10 lint(4)

--- src/zoo.js
+++ src/zoo.js
@@ -74,11 +74,11 @@ function entryCalculator(entrants = 0) {
 function schedule(dayName) {
   const semana = Object.keys(hours);
   const result = {};
-  semana.forEach((dia) => {
+  semana.map((dia) => {
     const { open, close } = hours[dia];
-    (dia === 'Monday')
+    return (dia === 'Monday'
       ? result[dia] = 'CLOSED'
-      : result[dia] = `Open from ${open}am until ${close - 12}pm`;
+      : result[dia] = `Open from ${open}am until ${close - 12}pm`);
   });
   if (dayName) return { [dayName]: result[dayName] };
 

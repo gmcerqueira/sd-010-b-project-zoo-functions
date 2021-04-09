149518ca654f7e14444e7ecbe0009ae348cad1a6
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-09 07:35:08 -0300

    animalsByIds done

--- src/zoo.js
+++ src/zoo.js
@@ -12,56 +12,80 @@ eslint no-unused-vars: [
 // Importando informações sobre o Zoológico
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const {
+  animals,
+  /* employees,
+  hours,
+  prices, */
+} = data;
+
+// Retorna um animal pelo seu id. Pode ser passado mais de um id.
+function animalsByIds(...ids) {
+  // Filtrar o obj 'animals' pelo 'id' recebido.
+  // Verificar se o 'id' do parametro é igual ao 'id' do animal.
+  // Utilizar 'some' em vez de 'find', pois some retorna bool e find retorna o objeto.
+  return animals.filter((animal) =>
+    ids.some((id) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return [animal, age];
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  return employeeName;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return [personalInfo, associatedWith];
 }
 
 function isManager(id) {
   // seu código aqui
+  return id;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  return [id, firstName, lastName, managers, responsibleFor];
 }
 
 function animalCount(species) {
   // seu código aqui
+  return species;
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  return entrants;
 }
 
 function animalMap(options) {
   // seu código aqui
+  return options;
 }
 
 function schedule(dayName) {
   // seu código aqui
+  return dayName;
 }
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  return id;
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  return percentage;
 }
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  return idOrName;
 }
 
 module.exports = {

1a1265a387911598becc5e72a072906e42ae6b72
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-04-09 16:11:30 -0300

    AddEmployee: test 5 and 6

--- src/zoo.js
+++ src/zoo.js
@@ -76,7 +76,7 @@ function isManager(id) {
 }
 // id, firstName, lastName, managers = [], responsibleFor
 // Requirement 6
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
   // console.log(data.employees.length);
   const newEmployee = {
@@ -84,19 +84,25 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor) {
     firstName,
     lastName,
     managers,
+    responsibleFor,
   };
   data.employees.push(newEmployee);
-  // console.log(newEmployee);
+  console.log(newEmployee);
   // console.log(data.employees.length);
   // const last = data.employees[8];
   // console.log(last.id);
   return newEmployee;
 }
-// const id1 = '39800c14-4b76-454a-858d-2f8d168146a7';
-// const n = 'John';
-// const s = 'Doe';
-// const vec = ['1', '2', '3'];
-// addEmployee(id1, n, s);
+
+// addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+//   [
+//     '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+//     'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+//   ],
+//   [
+//     'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+//     '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+//   ]);
 
 // function animalCount(species) {
 //   // seu código aqui
--- test/addEmployee.test.js
+++ test/addEmployee.test.js
@@ -12,18 +12,18 @@ describe('Implemente a função addEmployee', () => {
     assert.strictEqual(lastEmployee.firstName, 'John');
     assert.strictEqual(lastEmployee.lastName, 'Doe');
     assert.deepStrictEqual(lastEmployee.managers, []);
-    // assert.deepStrictEqual(lastEmployee.responsibleFor, []);
+    assert.deepStrictEqual(lastEmployee.responsibleFor, []);
 
-    // zoo.addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
-    //   [
-    //     '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
-    //     'a67a36ee-3765-4c74-8e0f-13f881f6588a',
-    //   ],
-    //   [
-    //     'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
-    //     '210fcd23-aa7b-4975-91b7-0230ebb27b99',
-    //   ]);
-    // assert.strictEqual(data.employees.length, 10);
+    zoo.addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+      [
+        '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+        'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+      ],
+      [
+        'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+        '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+      ]);
+    assert.strictEqual(data.employees.length, 10);
 
     // lastEmployee = data.employees[9];
     // assert.strictEqual(lastEmployee.id, '4141da1c-a6ed-4cf7-90c4-99c657ba4ef3');

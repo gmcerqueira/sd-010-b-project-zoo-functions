ce3ec0db479e1ac91a79494104c2758f9b52c30a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-09 17:28:30 -0300

    exercicios 1 e 4

--- src/zoo.js
+++ src/zoo.js
@@ -8,71 +8,62 @@ eslint no-unused-vars: [
   }
 ]
 */
-
 const data = require('./data');
+const {animals, employees, prices, hours } = data;
 
-function animalsByIds(ids) {
-  // seu código aqui
-  return ids;
+
+function animalsByIds(...ids) {
+   
+  return animals.filter((idAnimals) => idAnimals === ids); 
 }
+  
+  console.log(animalsByIds());
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
   return animal, age;
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
-  return employeeByName;
+  return employeeName;
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-  return personalInfo, associatedWith;
+  return {...personalInfo , ...associatedWith};
 }
 
 function isManager(id) {
-  // seu código aqui
   return id;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
   return id, firstName, lastName, managers, responsibleFor;
 }
 
 function animalCount(species) {
-  // seu código aqui
   return species;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
   return entrants;
 }
 
 function animalMap(options) {
-  // seu código aqui
   return options;
 }
 
 function schedule(dayName) {
-  // seu código aqui
   return dayName;
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
   return id;
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
   return percentage;
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
   return idOrName;
 }
 

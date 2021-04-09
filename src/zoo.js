5bf1aa2eb63e8030652c131900e85091bff5f974
Camila <camilafabiabh9@gmail.com>
2021-04-09 18:58:53 -0300

    atualizando

--- src/zoo.js
+++ src/zoo.js
@@ -11,42 +11,58 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees } = data;
+const { animals, employees, hours, prices } = data;
 function animalsByIds(...ids) {
   return animals.filter((animal) => ids.some((verificaId) => animal.id === verificaId));
+  // Usei o spread no parametro da função para retorna os ids,
+  // o filter para passar pelo array animals, e o some para retorna ao menos uma condição
+  // Pesquisei e tive ajuda durante o projeto. 
 }
 // console.log(animalsByIds())
 function animalsOlderThan(animal, age) {
   const retorno = animals.find((animalD) => animalD.name === animal);
   return retorno.residents.every((ageD) => ageD.age >= age);
+  // Usei o find para trazer a primeira condição dentro do array animals,
+  // o every para verificar se toda condição age, dentro do objeto residents tem idade minima.
 }
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};
-  const name = employeeName;
-  return employees.find((nameEmpl) => (name === nameEmpl.firstName || name === nameEmpl.lastName));
+  return employees.find((nameEmpl) => 
+  (employeeName === nameEmpl.firstName || employeeName === nameEmpl.lastName));
+  // Usei o find para retorna o objeto tanto pelo primeiro nome e último nome. 
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return { ...personalInfo, ...associatedWith };
+  const newEmployee = { ...personalInfo, ...associatedWith };
+  return newEmployee;
+  // Usei o spread para criar o novo colaborador.
 }
 
 function isManager(id) {
   return employees.some((idManager) => idManager.managers.includes(id));
+  // Usei o includes para verifica se dentro do array managers tem o item esperado 'id'.
+  // o some para retorna a condição do array employees.
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   return employees.push({ id, firstName, lastName, managers, responsibleFor });
+  // Troquei os parametros managers e responsibleFor adicionando Values de arrays.
+  // e utilizei o push para adicionar um novo objeto no array employees.
 }
 /*
 function animalCount(species) {
   // seu código aqui
 }
-
-function entryCalculator(entrants) {
-  // seu código aqui
+*/
+function entryCalculator(entrants = 0) {
+  let total = 0;
+  if (entrants.Adult) total += entrants.Adult * prices.Adult;
+  if (entrants.Senior) total += entrants.Senior * prices.Senior;
+  if (entrants.Child) total += entrants.Child * prices.Child;
+  return total;
 }
-
+/*
 function animalMap(options) {
   // seu código aqui
 }
@@ -68,7 +84,7 @@ function employeeCoverage(idOrName) {
 }
 */
 module.exports = {
-  // entryCalculator,
+  entryCalculator,
   // schedule,
   // animalCount,
   // animalMap,

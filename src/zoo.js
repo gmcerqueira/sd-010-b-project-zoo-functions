9831f86fd40f9790d8fd5336507f508855e76aab
Felippe Correa <felippecorrea2010@gmail.com>
2021-04-09 15:52:47 -0300

    Corrigindo eslint  requisito oldestFRomFirstSpecies 2

--- src/zoo.js
+++ src/zoo.js
@@ -96,8 +96,7 @@ function oldestFromFirstSpecies(id) {
   const { animals, employees } = data;
   const employ = employees.find((employer) => employer.id === id).responsibleFor[0];
   const animal = animals.find((ani) => ani.id === employ);
-  const result = animal.residents.reduce(
-    (acc, curr) => (curr.age > acc.age) ? curr : acc);
+  const result = animal.residents.reduce((acc, curr) => ((curr.age > acc.age) ? curr : acc));
   return [result.name, result.sex, result.age];
 }
 

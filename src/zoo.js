84f1b103232977d217cba60c2d8d75168e48343e
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-04-08 22:07:02 -0300

    CreateEmployee: test 1

--- src/zoo.js
+++ src/zoo.js
@@ -37,12 +37,31 @@ function employeeByName(employeeName) {
 
   return obj || {};
 }
-const emp = 'Wishart';
-console.log(employeeByName(emp));
+// const emp = 'Wishart';
+// console.log(employeeByName(emp));
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+  return Object.assign(personalInfo, associatedWith);
+}
+// const info = {
+//   id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+//   firstName: 'John',
+//   lastName: 'Doe',
+// };
+
+// const ass = {
+//   managers: [
+//     'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//     '9e7d4524-363c-416a-8759-8aa7e50c0992',
+//   ],
+//   responsibleFor: [
+//     '0938aa23-f153-4937-9f88-4858b24d6bce',
+//     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+//   ],
+// };
+// console.log(createEmployee(info, ass));
 
 // function isManager(id) {
 //   // seu código aqui
@@ -93,5 +112,5 @@ module.exports = {
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };

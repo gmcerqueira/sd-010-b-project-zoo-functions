97a8279ab74c89fecbfcd0816bd61820582f783f
Dieghonm <dieghonm@gmail.com>
2021-04-09 01:05:47 -0300

    Requisito 5, tentativa 1

--- src/zoo.js
+++ src/zoo.js
@@ -36,9 +36,9 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((empregado) => empregado.managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -81,7 +81,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

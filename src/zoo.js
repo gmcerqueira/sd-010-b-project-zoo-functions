46f61201a860375eaecc4a37b0948e761c1f821d
Lucas Ribeiro <lucasrb04@gmail.com>
2021-04-08 23:36:54 -0300

    realizando funçao isManager - requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -34,9 +34,14 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
+function isManager(id) {
+  const managers = [stephanieId, olaId, burlId];
+  return managers.some((ids) => ids === id);
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -79,7 +84,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

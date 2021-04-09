99b8ab01805e8c03c89edfe7fb77209f83bf7c91
Lucas Ribeiro <lucasrb04@gmail.com>
2021-04-08 23:18:30 -0300

    adicionando funçao createEmployee - requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -29,9 +29,10 @@ function employeeByName(employeeName) {
   }
   return result;
 }
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+
+function createEmployee(personalInfo, associatedWith) {
+  return { ...personalInfo, ...associatedWith };
+}
 
 // function isManager(id) {
 //   // seu código aqui
@@ -82,5 +83,5 @@ module.exports = {
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };

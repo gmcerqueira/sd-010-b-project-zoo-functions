6f448878cc7c6cb4206d754f08970bdebc42d549
Fernando <fhomaia@gmail.com>
2021-04-08 21:45:49 -0300

    Erros Lint

--- src/zoo.js
+++ src/zoo.js
@@ -78,86 +78,90 @@ function entryCalculator(entrants = undefined) {
 
 function animalMap(options = 'empty') {
   // seu código aqui
-  let animalsNe = data.animals.filter((animal) => animal.location === 'NE');
-  let animalsNw =data.animals.filter((animal) => animal.location === 'NW');
-  let animalsSe =data.animals.filter((animal) => animal.location === 'SE');
-  let animalsSw =data.animals.filter((animal) => animal.location === 'SW');
+  const animalsNe = data.animals.filter((animal) => animal.location === 'NE');
+  const animalsNw = data.animals.filter((animal) => animal.location === 'NW');
+  const animalsSe = data.animals.filter((animal) => animal.location === 'SE');
+  const animalsSw = data.animals.filter((animal) => animal.location === 'SW');
   if (options === 'empty' || options.includeNames !== true) {
     return {
-    NE: animalsNe.map((animal) => animal.name),
-    NW: animalsNw.map((animal) => animal.name),
-    SE: animalsSe.map((animal) => animal.name),
-    SW: animalsSw.map((animal) => animal.name),
+      NE: animalsNe.map((animal) => animal.name),
+      NW: animalsNw.map((animal) => animal.name),
+      SE: animalsSe.map((animal) => animal.name),
+      SW: animalsSw.map((animal) => animal.name),
     };
-  };
-  if(options.includeNames === true && options.sex === 'female' && options.sorted === true) {
+  }
+  if (options.includeNames === true && options.sex === 'female' && options.sorted === true) {
     return {
-      NE: animalsNe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name).sort()})),
-      NW: animalsNw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name).sort()})),
-      SE: animalsSe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name).sort()})),
-      SW: animalsSw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name).sort()})),
-      };
-  };
-  if(options.includeNames === true && options.sex === 'male' && options.sorted === true) {
+      NE: animalsNe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name).sort() })),
+      NW: animalsNw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name).sort() })),
+      SE: animalsSe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name).sort() })),
+      SW: animalsSw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name).sort() })),
+    };
+  }
+  if (options.includeNames === true && options.sex === 'male' && options.sorted === true) {
     return {
-      NE: animalsNe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name).sort()})),
-      NW: animalsNw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name).sort()})),
-      SE: animalsSe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name).sort()})),
-      SW: animalsSw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name).sort()})),
+      NE: animalsNe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name).sort() })),
+      NW: animalsNw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name).sort() })),
+      SE: animalsSe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name).sort() })),
+      SW: animalsSw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name).sort() })),
       };
-  };
+  }
   if (options.includeNames === true && options.sorted === true) {
     return {
-      NE: animalsNe.map((animal) => ({[animal.name]: animal.residents.map((resident) => 
-        resident.name).sort()})),
-      NW: animalsNw.map((animal) => ({[animal.name]: animal.residents.map((resident) => 
-        resident.name).sort()})),
-      SE: animalsSe.map((animal) => ({[animal.name]: animal.residents.map((resident) => 
-        resident.name).sort()})),
-      SW: animalsSw.map((animal) => ({[animal.name]: animal.residents.map((resident) => 
-        resident.name).sort()})),
+      NE: animalsNe.map((animal) => ({ [animal.name]: animal.residents.map((resident) =>
+        resident.name).sort() })),
+      NW: animalsNw.map((animal) => ({ [animal.name]: animal.residents.map((resident) =>
+        resident.name).sort() })),
+      SE: animalsSe.map((animal) => ({ [animal.name]: animal.residents.map((resident) =>
+        resident.name).sort() })),
+      SW: animalsSw.map((animal) => ({ [animal.name]: animal.residents.map((resident) =>
+        resident.name).sort() })),
       };
-  };
-  if(options.includeNames === true && options.sex === 'female') {
+  }
+  if (options.includeNames === true && options.sex === 'female') {
     return {
-      NE: animalsNe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name)})),
-      NW: animalsNw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name)})),
-      SE: animalsSe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name)})),
-      SW: animalsSw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'female').map((resident) => resident.name)})),
+      NE: animalsNe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name) })),
+      NW: animalsNw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name) })),
+      SE: animalsSe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name) })),
+      SW: animalsSw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'female').map((resident) => resident.name) })),
       };
-  };
-  if(options.includeNames === true && options.sex === 'male') {
+  }
+  if (options.includeNames === true && options.sex === 'male') {
     return {
-      NE: animalsNe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name)})),
-      NW: animalsNw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name)})),
-      SE: animalsSe.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name)})),
-      SW: animalsSw.map((animal) => ({[animal.name]: animal.residents.filter((resident) => 
-        resident.sex === 'male').map((resident) => resident.name)})),
+      NE: animalsNe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name) })),
+      NW: animalsNw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name) })),
+      SE: animalsSe.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name) })),
+      SW: animalsSw.map((animal) => ({ [animal.name]: animal.residents.filter((resident) =>
+        resident.sex === 'male').map((resident) => resident.name) })),
       };
-  };
+  }
   if (options.includeNames === true) {
     return {
-      NE: animalsNe.map((animal) => ({[animal.name]: animal.residents.map((resident) => resident.name)})),
-      NW: animalsNw.map((animal) => ({[animal.name]: animal.residents.map((resident) => resident.name)})),
-      SE: animalsSe.map((animal) => ({[animal.name]: animal.residents.map((resident) => resident.name)})),
-      SW: animalsSw.map((animal) => ({[animal.name]: animal.residents.map((resident) => resident.name)})),
+      NE: animalsNe.map((animal) =>
+      ({ [animal.name]: animal.residents.map((resident) => resident.name) })),
+      NW: animalsNw.map((animal) =>
+      ({ [animal.name]: animal.residents.map((resident) => resident.name) })),
+      SE: animalsSe.map((animal) =>
+      ({ [animal.name]: animal.residents.map((resident) => resident.name) })),
+      SW: animalsSw.map((animal) =>
+      ({ [animal.name]: animal.residents.map((resident) => resident.name) })),
       };
-  };
+  }
 }
 // console.log(animalMap({includeNames :true}))
 

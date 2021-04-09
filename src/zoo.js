9b416a52cdbe36ae91618b273e69e99379b05266
DanielTrybe <darkdanielr@gmail.com>
2021-04-08 21:25:32 -0300

    ate o 5

--- src/zoo.js
+++ src/zoo.js
@@ -9,27 +9,58 @@ eslint no-unused-vars: [
 ]
 */
 
+const {
+  animals,
+  employees,
+} = require('./data');
 const data = require('./data');
+// let idade = data.animals;
+// console.log(idade)
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(ids, ids2) {
+  if (ids2 !== undefined) {
+    const animal1 = data.animals.filter((animal) => (animal.id === ids));
+    const animal2 = data.animals.filter((animal) => (animal.id === ids2));
+    return animal1.concat(animal2);
+  }
+  return data.animals.filter((animal) => (animal.id === ids));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const searchBixo = animals.find((bixo) => bixo.name === animal).residents;
+  return searchBixo.every((anim) => anim.age >= age);
 }
 
+// Feito com auxilio de Rafael Gumieri
+
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find((emp) => (emp.firstName === employeeName || emp.lastName === employeeName));
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const bodega = {
+    ...personalInfo,
+    ...associatedWith,
+  };
+  return bodega;
 }
 
 function isManager(id) {
-  // seu código aqui
+  let sadBoy = 0;
+  employees.filter((gerente) => gerente.managers.forEach((item) => {
+    if (item === id) {
+      sadBoy += 1;
+    }
+  }));
+  if (sadBoy > 0) {
+    return true;
+  }
+  return false;
 }
+// Pedro Henrique e eu fizemos juntos
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui

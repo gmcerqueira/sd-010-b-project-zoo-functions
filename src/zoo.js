a01f9ccfc6fbbd59645b663979f0b1ec86d7bb86
Jessie <miss_sc_jessi@hotmail.com>
2021-04-09 18:33:35 -0300

    requisito 11 lint(4)

--- src/zoo.js
+++ src/zoo.js
@@ -93,9 +93,9 @@ function oldestFromFirstSpecies(id) {
     .responsibleFor.find((animalId) => animalId);
   const animalMaisVelho = animals
     .find((idAnimal) => idAnimal.id === primeiroAnimal).residents
-    .reduce((maisVelho, idadeAnimal) => {
-      return (maisVelho.age > idadeAnimal.age) ? maisVelho : idadeAnimal;
-    });
+    .reduce((maisVelho, idadeAnimal) => 
+    ((maisVelho.age > idadeAnimal.age) ? maisVelho : idadeAnimal)
+  );
   return Object.values(animalMaisVelho);
 }
 

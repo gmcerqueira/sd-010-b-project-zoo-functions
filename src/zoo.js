604f530d34fbd523f15126f701d9dbe917653372
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-08 21:34:19 -0300

    :fire: Desafios Insanos

--- src/zoo.js
+++ src/zoo.js
@@ -18,11 +18,11 @@ function animalsByIds(...ids) {
   // return teste;
   // return animals.filter((animal) => (animal.id === ids)),{}
   const {
-    animals
+    animals,
   } = data;
   if (ids[1] !== undefined) {
     const idAnimal1 = animals.filter((animal) => (animal.id === ids[0]));
-    const idAnimal2 = animals.filter((animal) => (animal.id === ids[1]))
+    const idAnimal2 = animals.filter((animal) => (animal.id === ids[1]));
     return idAnimal1.concat(idAnimal2);
   }
   return animals.filter((animal) => (animal.id === ids[0]));
@@ -32,76 +32,93 @@ function animalsOlderThan(animal, age) {
   // seu código aqui
   // 1º buscar o nome de todas as especie,2° puxar de uma especie separada, 3° criar parametro de idade minima para ser puxado, 4°
   const procuraAnimal = data.animals.find((furry) => (furry.name === animal));
-  const procuraIdade = procuraAnimal.residents.map((idade) => ((idade.age >= age) ? true : false));
-  return procuraIdade.every((idades) => (idades === true))
+  const procuraIdade = procuraAnimal.residents.map((idade) => ((idade.age >= age)));
+  return procuraIdade.every((idades) => (idades === true));
   // return teste;
-};
+}
 
 function employeeByName(employeeName) {
   // seu código aqui
-  //Agradecimentos ao Rafael Guimieri
+  // Agradecimentos ao Rafael Guimieri
   const {
-    employees
+    employees,
   } = data;
   if (employeeName !== undefined) {
-    return employees.find((name) => ((name.firstName === employeeName || name.lastName === employeeName)));
+    return employees.find((nm) => (nm.firstName === employeeName || nm.lastName === employeeName));
   }
   return {};
 }
 console.log(employeeByName('Nelson'));
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith};
+  return {
+    ...personalInfo,
+    ...associatedWith,
+  };
 }
 
 function isManager(id) {
   // seu código aqui
+  // 1° verificar 1 pessoa dado o parametro
+  // 2°verificar o seu cargo de gerencia
+  // let resultado = data.employees.filter((element) => element.managers.forEach((item) => (item === id) ? true : false));
+  // return resultado;
+  let cont = 0;
+  data.employees.filter((manager) => manager.managers.forEach((item) => {
+    if (item === id) {
+      cont += 1;
+    }
+  }));
+  if (cont > 0) {
+    return true;
+  }
+  return false;
 }
+// Concluido com a Colaboração de Daniel ROberto
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function animalCount(species) {
+//   // seu código aqui
+// }
 
-function animalCount(species) {
-  // seu código aqui
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
-
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  // entryCalculator,
+  // schedule,
+  // animalCount,
+  // animalMap,
   animalsByIds,
   employeeByName,
-  employeeCoverage,
-  addEmployee,
+  // employeeCoverage,
+  // addEmployee,
   isManager,
   animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
+  // oldestFromFirstSpecies,
+  // increasePrices,
   createEmployee,
 };

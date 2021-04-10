b88bd0a8bfa09e63cdc7b339b7f1b177ab274b46
tonpasso <wellpasso@gmail.com>
2021-04-10 10:14:13 -0300

    Requisito 08

--- src/zoo.js
+++ src/zoo.js
@@ -49,7 +49,8 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  return entrants;
+  if (!entrants || entrants === {}) return 0;
+  return Object.keys(entrants).reduce((acc, curr) => acc + data.prices[curr] * entrants[curr], 0);
 }
 
 function animalMap(options) {

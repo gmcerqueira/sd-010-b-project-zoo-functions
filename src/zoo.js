a5d84ca73c189cafbea36f560549f1ec54c7af2d
Vinicius <vini_bodra@hotmail.com>
2021-04-10 10:10:47 -0300

    Implementando animalCount

--- src/zoo.js
+++ src/zoo.js
@@ -62,20 +62,21 @@ function animalCount(species) {
   // seu código aqui
   // this: https://dev.to/_bigblind/quick-tip-transform-an-array-into-an-object-using-reduce-2gh6 helped me A LOT!
   const allSpecies = animals.reduce((acc, { name, residents }) => {
-    return { ...acc, [name]: residents.length };
+    const resultado = ({ ...acc, [name]: residents.length });
+    return resultado;
   }, {});
 
   const oneSpecie = animals.find((item) => {
     if (item.name === species) {
       return item;
-    }
+    } return 'a';
   });
 
   if (!species) {
     return allSpecies;
   } return oneSpecie.residents.length;
 }
-console.log(animalCount());
+
 // function entryCalculator(entrants) {
 //   // seu código aqui
 // }

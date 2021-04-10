fc0c450582b93df57a3ec98903ec8fe383a07261
DanielTrybe <darkdanielr@gmail.com>
2021-04-10 14:16:59 -0300

    nao sei qual parei

--- src/zoo.js
+++ src/zoo.js
@@ -12,6 +12,8 @@ eslint no-unused-vars: [
 const {
   animals,
   employees,
+  prices,
+  hours,
 } = require('./data');
 const data = require('./data');
 // let idade = data.animals;
@@ -62,50 +64,131 @@ function isManager(id) {
 }
 // Pedro Henrique e eu fizemos juntos
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  const inserir = data.employees;
+  const empregado = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  }
+  if (managers === undefined) {
+    empregado.managers = []
+  }
+  if (responsibleFor === undefined) {
+    empregado.responsibleFor = []
+  }
+  return inserir.push(empregado);
+}
+
+function animalCount(species) {
+  if (species !== undefined) {
+    return data.animals.find((bixo) => bixo.name === species).residents.length;
+  }
+  if (species === undefined) {
+    return data.animals.reduce((bixoAcc, quanti) => {
+      bixoAcc[quanti.name] = quanti.residents.length;
+      return bixoAcc;
+    }, {});
+  }
+}
+
+function entryCalculator(entrants) {
+  if (entrants === undefined) {
+    return 0;
+  }
+  const treco = Object.keys(entrants);
+  let total = 0;
+  treco.forEach((key) => {
+    const result = data.prices[key] * entrants[key];
+    total += result;
+  });
+  return total;
+}
+// ---------------------------------------------------------------------------------------
+// function semPar() {
+//   return data.animals.reduce((bixoAcc, quanti) => {
+//     if (!bixoAcc[quanti.location]) {
+//       bixoAcc[quanti.location] = [];
+//     }
+//     bixoAcc[quanti.location].push(quanti.name);
+//     return bixoAcc;
+//   }, {});
 // }
 
-// function animalCount(species) {
-//   // seu código aqui
-// }
-
-// function entryCalculator(entrants) {
-//   // seu código aqui
+// function puxaLocal() {
+//   return data.animals.reduce((bixoAcc, quanti) => {
+//     if (!bixoAcc[quanti.location]) {
+//       bixoAcc[quanti.location] = [];
+//     }
+//     bixoAcc[quanti.location].push(quanti.name);
+//     return bixoAcc;
+//   }, {});
 // }
 
 // function animalMap(options) {
-//   // seu código aqui
+//   if (!options) {
+//     return semPar();
+//   }
+//   const obj = Object.keys(options);
+//   if (obj.includes('includeNames')) {
+//     return data.animals.reduce((bixoAcc, quanti) => {
+//       if (!bixoAcc[quanti.name]) {
+//         bixoAcc[quanti.name] = [];
+//       }
+//       quanti.residents.forEach((resident) => bixoAcc[quanti.name].push(resident.name));
+//       return bixoAcc;
+//     }, {});
+//   }
 // }
-
-// function schedule(dayName) {
-//   // seu código aqui
-// }
+// // console.log(animalMap({ includeNames: true }))
+// ------------------------------------------------------------------------------------------------------
+function schedule(dayName) {
+    let seila = Object.keys(hours);
+    let seila2 = Object.values(hours);
+    let total = {};
+    seila.forEach((dat, index) =>
+    total[dat] = `Open from ${seila2[index].open}am until ${seila2[index].close - 12}pm`);
+    total.Monday = 'CLOSED';
+    if (!dayName) {
+      return total;
+    }
+    return {[dayName]: total[dayName]}; // notação de ponto não funciona para váriaveis
+}
 
 // function oldestFromFirstSpecies(id) {
 //   // seu código aqui
 // }
 
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
-
+function increasePrices(percentage) {
+  let {
+    Adult,
+    Senior,
+    Child
+  } = prices;
+  let porce = percentage / 100;
+  prices.Adult = Math.round(((Adult * porce) + Adult) * 100) / 100;
+  prices.Senior = Math.round(((Senior * porce) + Senior) * 100) / 100;
+  prices.Child = Math.round(((Child * porce) + Child) * 100) / 100;
+}
+// aradecimento ao Lucas Martins
 // function employeeCoverage(idOrName) {
 //   // seu código aqui
 // }
 
 module.exports = {
-  // entryCalculator,
-  // schedule,
-  // animalCount,
+  entryCalculator,
+  schedule,
+  animalCount,
   // animalMap,
   animalsByIds,
   employeeByName,
   // employeeCoverage,
-  // addEmployee,
+  addEmployee,
   isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
-  // increasePrices,
+  increasePrices,
   createEmployee,
 };

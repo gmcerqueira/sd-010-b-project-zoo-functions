42efd10df1bbf6e677c8ffc16f569588bc19de08
Carlos Vieira <appencigr@gmail.com>
2021-04-10 13:41:00 -0300

    Requisito 2 Feito

--- src/zoo.js
+++ src/zoo.js
@@ -9,74 +9,83 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals, employees } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) { // ...ids
   // seu código aqui
-  [ids] = data.animals;
-  return ids;
+  if (ids.length === 0) {
+    return [];
+  }
+  return animals.filter((idAnimal) => (ids.includes(idAnimal.id)));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  const specieAnimal = animals.find((animalName) => animalName.name === animal);
+
+  return specieAnimal.residents.every((animalAge) => animalAge.age >= age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  return employeeByName;
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+//   return personalInfo + associatedWith;
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+//   return id;
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+// //   // seu código aqui
+// // }
 
-function animalCount(species) {
-  // seu código aqui
-}
+// function animalCount(species) {
+//   // seu código aqui
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+// }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  // entryCalculator,
+  // schedule,
+  // animalCount,
+  // animalMap,
   animalsByIds,
   employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
+  // employeeCoverage,
+  // addEmployee,
+  // isManager,
   animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  // oldestFromFirstSpecies,
+  // increasePrices,
+  // createEmployee,
 };

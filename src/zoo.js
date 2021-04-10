8c043066607131a0d72f4fceef37b77e8202b801
Igor Machado <rf59imft@gmail.com>
2021-04-10 07:24:38 -0300

    DESAFIO 01

--- src/zoo.js
+++ src/zoo.js
@@ -11,10 +11,27 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+const { animals } = require('./data');
+
+function animalsByIds(...ids) {
   // seu código aqui
-  // vamos com tudo!!
+  // o filter vai retornar um objeto contendo todas as infos
+  // do animal
+  // foi realizado o find dentro do filter para especificar
+  // que a busca pela info do animal será pelo id.
+  const findIdAnimals = animals.filter((animal) =>
+    ids.find((element) => element === animal.id),
+  );
+  return findIdAnimals;
 }
+
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(
+  animalsByIds(
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+  ),
+);
 
 function animalsOlderThan(animal, age) {
   // seu código aqui

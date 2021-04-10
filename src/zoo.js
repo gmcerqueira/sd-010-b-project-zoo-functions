29b151d1f6b6d0820294de0602a2755b38c585d7
Aremtior <marcosleandrofl@gmail.com>
2021-04-10 20:48:30 -0300

    requisito 13 - employeeCoverage function fixed.

--- src/zoo.js
+++ src/zoo.js
@@ -9,17 +9,17 @@ eslint no-unused-vars: [
 ]
 */
 
-const { employees } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
-  return data.animals.filter((a) => ids.find((id) => a.id === id));
+  return animals.filter((a) => ids.find((id) => a.id === id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  return data.animals.find((a) => a.name === animal).residents.every((r) => r.age >= age);
+  return animals.find((a) => a.name === animal).residents.every((r) => r.age >= age);
 }
 
 function employeeByName(employeeName) {
@@ -37,20 +37,20 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  return data.employees.some((e) => e.managers.find((m) => m === id));
+  return employees.some((e) => e.managers.find((m) => m === id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
-  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
-  return data.employees;
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
+  return employees;
 }
 
 function animalCount(species) {
   if (species) {
-    return data.animals.find((animal) => animal.name === species).residents.length;
+    return animals.find((animal) => animal.name === species).residents.length;
   }
-  return data.animals.reduce((acc, currAnimal) => {
+  return animals.reduce((acc, currAnimal) => {
     const animalAcc = acc;
     animalAcc[currAnimal.name] = currAnimal.residents.length;
     return acc;
@@ -61,21 +61,21 @@ function entryCalculator(entrants) {
   // seu código aqui
   if (entrants) {
     const arrEntrants = Object.entries(entrants);
-    return arrEntrants.reduce((a, b) => a + b[1] * data.prices[b[0]], 0);
+    return arrEntrants.reduce((a, b) => a + b[1] * prices[b[0]], 0);
   }
   return 0;
 }
 
 function animalsByLoc() {
-  return data.animals.reduce((acc, b) => {
-    acc[b.location] = data.animals.filter((a) => a.location === b.location).map((a) => a.name);
+  return animals.reduce((acc, b) => {
+    acc[b.location] = animals.filter((a) => a.location === b.location).map((a) => a.name);
     return acc;
   }, {});
 }
 
 function animalsNames(sorted) {
-  return data.animals.reduce((acc, b) => {
-    const animalsByLocation = data.animals.filter((a) => a.location === b.location);
+  return animals.reduce((acc, b) => {
+    const animalsByLocation = animals.filter((a) => a.location === b.location);
     // console.log(animalsByLocation);
     acc[b.location] = animalsByLocation.reduce((anmAcc, curAnm) => {
       const residents = {};
@@ -93,8 +93,8 @@ function animalsNames(sorted) {
 }
 
 function animalsNamesSex(sex, sorted) {
-  return data.animals.reduce((acc, b) => {
-    const animalsByLocation = data.animals.filter((a) => a.location === b.location);
+  return animals.reduce((acc, b) => {
+    const animalsByLocation = animals.filter((a) => a.location === b.location);
     // console.log(animalsByLocation);
     acc[b.location] = animalsByLocation.reduce((anmAcc, curAnm) => {
       const residents = {};
@@ -132,12 +132,12 @@ function createsSentence({ open, close }) {
 function schedule(dayName) {
   // seu código aqui
   if (dayName) {
-    const info = Object.entries(data.hours).find((day) => day[0] === dayName);
+    const info = Object.entries(hours).find((day) => day[0] === dayName);
     const oneDay = {};
     oneDay[dayName] = createsSentence(info[1]);
     return oneDay;
   }
-  return Object.entries(data.hours).reduce((acc, curr) => {
+  return Object.entries(hours).reduce((acc, curr) => {
     acc[curr[0]] = createsSentence(curr[1]);
     return acc;
   }, {});
@@ -145,45 +145,69 @@ function schedule(dayName) {
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
-  const idSpecies = data.employees.find((e) => e.id === id).responsibleFor[0];
-  const speciesResidents = data.animals.find((animal) => animal.id === idSpecies).residents;
+  const idSpecies = employees.find((e) => e.id === id).responsibleFor[0];
+  const speciesResidents = animals.find((animal) => animal.id === idSpecies).residents;
   const older = speciesResidents.reduce((acc, r) => (acc > r.age ? acc : r.age), 0);
   return Object.values(speciesResidents.find((resident) => resident.age === older));
 }
 
 function increasePrices(percentage) {
   // seu código aqui (https://medium.com/swlh/how-to-round-to-a-certain-number-of-decimal-places-in-javascript-ed74c471c1b8#:~:text=For%20example%2C%20if%20you%20want,by%20100%20to%20get%200.23.)
-  console.log(Object.entries(data.prices));
-  Object.entries(data.prices).forEach((price) => {
-    data.prices[price[0]] = Math.round((price[1] + (price[1] * (percentage / 100))) * 100) / 100;
-    console.log(data.prices);
+  console.log(Object.entries(prices));
+  Object.entries(prices).forEach((price) => {
+    prices[price[0]] = Math.round((price[1] + (price[1] * (percentage / 100))) * 100) / 100;
+    console.log(prices);
   });
 }
 
 function employeesCoverage() {
-  return data.employees.reduce((acc, e) => {
+  return employees.reduce((acc, e) => {
     const fullName = `${e.firstName} ${e.lastName}`;
-    acc[fullName] = data.animals.filter((animal) => {
-      const animalNames = e.responsibleFor.find((animalId) => animal.id === animalId);
+    const arrNames = [];
+    e.responsibleFor.filter((animalId) => {
+      const animalNames = animals.find((animal) => animal.id === animalId);
+      arrNames.push(animalNames.name);
       return animalNames;
     }).map((a) => a.name);
+    acc[fullName] = arrNames;
     return acc;
   }, {});
 }
 
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+//   if (idOrName) {
+//     const employee = {};
+//     const emp =  employees.find((e) => {
+//       const find = e.id === idOrName || e.firstName === idOrName || e.lastName === idOrName;
+//       return find;
+//     });
+//     const fullName = `${emp.firstName} ${emp.lastName}`;
+//     employee[fullName] =  animals.filter((animal) => {
+//       const animalNames = emp.responsibleFor.find((animalId) => animal.id === animalId);
+//       console.log(animalNames);
+//       return animalNames;
+//     }).map((a) => a.name);
+//     return employee;
+//   }
+//   return employeesCoverage();
+// }
 function employeeCoverage(idOrName) {
   // seu código aqui
   if (idOrName) {
     const employee = {};
-    const emp = data.employees.find((e) => {
+    const emp = employees.find((e) => {
       const find = e.id === idOrName || e.firstName === idOrName || e.lastName === idOrName;
       return find;
     });
     const fullName = `${emp.firstName} ${emp.lastName}`;
-    employee[fullName] = data.animals.filter((animal) => {
-      const animalNames = emp.responsibleFor.find((animalId) => animal.id === animalId);
-      return animalNames;
-    }).map((a) => a.name);
+    const arrNames = [];
+    emp.responsibleFor.filter((animalId) => {
+      const animalNames = animals.find((animal) => animal.id === animalId);
+      arrNames.push(animalNames.name);
+      return animalNames.name;
+    });
+    employee[fullName] = arrNames;
     return employee;
   }
   return employeesCoverage();

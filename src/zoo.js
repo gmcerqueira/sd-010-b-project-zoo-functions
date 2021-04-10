9accce6583af5cb426af4aa51bbf7c884619862d
Valeriandreoni <valeriandreoni@gmail.com>
2021-04-10 14:38:37 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -36,9 +36,9 @@ function createEmployee(personalInfo, associatedWith) {
   return novoFuncionario;
 }
 
-/* function isManager(id) {
-  // seu código aqui
-} */
+function isManager(id) {
+  return funcionarios.some((emp) => emp.managers.includes(id));
+}
 
 /* function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
@@ -81,7 +81,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

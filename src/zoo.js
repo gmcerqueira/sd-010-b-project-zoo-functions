097d67849919990850a326f38281f4bb712809c5
Leandro Xavier <leandrosxavi@gmail.com>
2021-04-11 00:49:00 -0300

    Adiciona requisitos 1 ao 4. Requisito 1 ainda incompleto, teste Lint do Github

--- src/zoo.js
+++ src/zoo.js
@@ -9,72 +9,109 @@ eslint no-unused-vars: [
 ]
 */
 
+const {
+  animals,
+  employees,
+} = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  return ids;
+function animalsByIds(...ids) {
+  const animalsData = ids.map((id) => animals.find((animal) => (animal.id === id)));
+  return animalsData;
 }
 
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const findAnimal = animals.find((element) => element.name === animal).residents;
+  const verifyAge = findAnimal.every((value) => value.age >= age);
+  return verifyAge;
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  let employeeData = employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  if (employeeName === undefined) {
+    employeeData = {};
+  }
+  return employeeData;
 }
+
+// console.log(employeeByName('Orloff'));
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployeeData = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return newEmployeeData;
 }
+// console.log(createEmployee({
+//   id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+//   firstName: 'John',
+//   lastName: 'Doe',
+// }, {
+//   managers: [
+//     'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//     '9e7d4524-363c-416a-8759-8aa7e50c0992'
+//   ],
+//   responsibleFor: [
+//     '0938aa23-f153-4937-9f88-4858b24d6bce',
+//     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+//   ]
+// }));
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function animalCount(species) {
-  // seu código aqui
-}
+// function animalCount(species) {
+//   // seu código aqui
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+// }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  // entryCalculator,
+  // schedule,
+  // animalCount,
+  // animalMap,
   animalsByIds,
   employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
+  // employeeCoverage,
+  // addEmployee,
+  // isManager,
   animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
+  // oldestFromFirstSpecies,
+  // increasePrices,
   createEmployee,
 };

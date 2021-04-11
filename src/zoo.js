5d019f281db900919b4d46589a5bdce8699eea98
Daniel Ceci <danielceci96@gmail.com>
2021-04-11 15:13:49 -0300

    tentando fazer dar certo pelo menos os requisitos 1 e 2

--- src/zoo.js
+++ src/zoo.js
@@ -13,7 +13,7 @@ eslint no-unused-vars: [
 // OBS: no requisito 1 eu mantive os mesmos nomes de variáveis que o Alexandre porque achei que ele escolheu nomes perfeitos e concisos. Qualquer mudança de nome que eu fizesse só iria atrapalhar.
 const data = require('./data');
 
-const { employees } = data;
+// const { employees } = data;
 const { animals } = data;// é pra trazer as informações de animals que estão no data.js
 
 function animalsByIds(...ids) { // esse ids será um conjunto de vários ID(ou pode não ser também).
@@ -29,69 +29,69 @@ function animalsOlderThan(animal, age) {
     .residents.every((residente) => residente.age >= age); // o find vai encontrar em animals o primeiro objeto(bicho) que tiver o name igual ao que foi colocado no parâmetro. Descobrindo isso, logo depois acessa o residents deste objeto. Aí usa o every dentro do residents encontrado. O every retorna um booleano e dá true se TODOS atenderem alguma condição. Nesse caso, vai dar true se todo "residente"  tiver uma idade maior ou igual a idade colocada no parâmetro.
 }
 
-function employeeByName(employeeName) {
-  if (typeof employeeName === 'undefined') {
-    return {};
-  }
-  return employees.find((empregado) => {
-    if (empregado.firstName === employeeName || empregado.lastName === employeeName) {
-      return empregado;
-    }
-  });
-}
+// function employeeByName(employeeName) {
+//   if (typeof employeeName === 'undefined') {
+//     return {};
+//   }
+//   employees.find((empregado) => {
+//     if (empregado.firstName === employeeName || empregado.lastName === employeeName) {
+//       return empregado;
+//     }
+//   });
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
-
-function isManager(id) {
+// function createEmployee(personalInfo, associatedWith) {
 //   // seu código aqui
-}
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function isManager(id) {
+// //   // seu código aqui
+// }
 
-function animalCount(species) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function animalCount(species) {
+//   // seu código aqui
+// }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
+
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  // entryCalculator,
+  // schedule,
+  // animalCount,
+  // animalMap,
   animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
+  // employeeByName,
+  // employeeCoverage,
+  // addEmployee,
+  // isManager,
   animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  // oldestFromFirstSpecies,
+  // increasePrices,
+  // createEmployee,
 };

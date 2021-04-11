1144937e8c92d1b416fda264f5375fb3a0b8b457
Daniel Ceci <danielceci96@gmail.com>
2021-04-10 21:05:37 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -8,11 +8,18 @@ eslint no-unused-vars: [
   }
 ]
 */
+// requisitos 1,2 e 3 eu fiz com a ajuda do meu colega Alexandre Damasceno que me ajudou muito.  Link do PR:https://github.com/tryber/sd-010-b-project-zoo-functions/pull/66/commits
 
+// OBS: no requisito 1 eu mantive os mesmos nomes de variáveis que o Alexandre porque achei que ele escolheu nomes perfeitos e concisos. Qualquer mudança de nome que eu fizesse só iria atrapalhar.
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const { animals } = data;// é pra trazer as informações de animals que estão no data.js
+function animalsByIds(...ids) { // esse ids será um conjunto de vários ID(ou pode não ser também).
+  if (typeof (ids) === 'undefined') {
+    return [];
+  }
+  // função map retorna um array
+  return ids.map((id) => animals.find((animal) => animal.id === id)); // o find  vai encontrar o PRIMEIRO animal que tenha um id idêntico ao ID que tá escrito no parâmetro de animalByIds. Aí descobrindo isso, o MAP vai me retornar um array com o objeto do animal especifico.
 }
 
 function animalsOlderThan(animal, age) {

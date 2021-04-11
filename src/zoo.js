5d240ec2549335085bf57661b66f695f15c1c015
Leandro Xavier <leandrosxavi@gmail.com>
2021-04-11 03:17:28 -0300

    Adiciona requisitos 5 e 6. Comecei o 7, mas ainda esta incompleto

--- src/zoo.js
+++ src/zoo.js
@@ -64,17 +64,39 @@ function createEmployee(personalInfo, associatedWith) {
 //   ]
 // }));
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const burl = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+  const ola = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+  const stephanie = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+  const managerList = [burl, ola, stephanie];
+  const findEmployee = employees.find((employee) => employee.id === id);
+  return managerList.some((manager) => findEmployee.id === manager);
+}
+// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployeeData = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  return employees.push(newEmployeeData);
+}
+
 
 // function animalCount(species) {
-//   // seu código aqui
+//   const object = {};
+//   const animalList = animals.reduce((acc, curr) => acc['nome'] = curr.name, {});
+//   if (species === undefined) {
+//     return animalList;
+//   } else {
+//     return animals.find((animal) => animal.name === species).residents.length;
+//   }
 // }
+
+// console.log(animalCount());
 
 // function entryCalculator(entrants) {
 //   // seu código aqui
@@ -108,8 +130,8 @@ module.exports = {
   animalsByIds,
   employeeByName,
   // employeeCoverage,
-  // addEmployee,
-  // isManager,
+  addEmployee,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

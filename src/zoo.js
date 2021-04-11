058488f0ec5d7ecdf7fb7d5237f1a414de208ff9
riquee <strikedv@hotmail.com>
2021-04-11 15:37:50 -0300

    Finalizado todas refatoracoes

--- src/zoo.js
+++ src/zoo.js
@@ -19,9 +19,9 @@ const animalsOlderThan = (animal, ageAnimal) =>
   search(animals, animal, 'name').residents.every(({ age }) => age > ageAnimal);
 
 const employeeByName = (employeeName) =>
-  employeeName
+  (employeeName
     ? employees.find((e) => e.firstName === employeeName || e.lastName === employeeName)
-    : {};
+    : {});
 
 const createEmployee = (arg1, arg2) => ({ ...arg1, ...arg2 });
 
@@ -38,16 +38,33 @@ const animalCount = (species) => {
   return species ? list[species] : list;
 };
 
-function entryCalculator(entrants) {
+const entryCalculator = (entrants) => {
   if (!entrants || entrants === {}) return 0;
   const { Adult, Child, Senior } = prices;
   const { Adult: a = 0, Child: b = 0, Senior: c = 0 } = entrants;
   return Adult * a + Child * b + Senior * c;
-}
+};
 
-function animalMap(options) {
-  // seu código aqui
-}
+const listModel = (callback, sex, sorted) => {
+  const list = { NE: [], NW: [], SE: [], SW: [] };
+  Object.keys(list).forEach((key) => {
+    const animal = animals.filter(({ location }) => key === location);
+    list[key] = callback(animal, sex, sorted);
+  });
+  return list;
+};
+
+const listIncludes = (animal, sex, sorted) =>
+  animal.map(({ name, residents }) => {
+    const arr = residents.filter((e) => e.sex === sex || !sex).map((e) => e.name);
+    return sorted ? { [name]: arr.sort() } : { [name]: arr };
+  });
+
+const animalMap = (options) => {
+  const { includeNames, sex, sorted } = options || {};
+  if (!options || !includeNames) return listModel((animal) => animal.map(({ name }) => name));
+  return listModel(listIncludes, sex, sorted);
+};
 
 const schedule = (dayName) => {
   const list = {};
@@ -58,20 +75,28 @@ const schedule = (dayName) => {
 };
 
 const oldestFromFirstSpecies = (idEmploye) => {
-  const { responsibleFor } = search(employees, idEmploye, 'id');
-  const { residents } = search(animals, responsibleFor[0], 'id');
+  const { responsibleFor } = employees.find(({ id }) => id === idEmploye);
+  const { residents } = animals.find(({ id }) => id === responsibleFor[0]);
   const { name, sex, age } = residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
   return [name, sex, age];
-}
+};
 
 const increasePrices = (percentage) => {
   Object.keys(prices).forEach((key) => {
     prices[key] += (prices[key] * percentage) / 100;
     prices[key] = Math.round(prices[key] * 100) / 100;
   });
-}
+};
 
-function employeeCoverage(idOrName) {}
+function employeeCoverage(idOr) {
+  const list = {};
+  employees.forEach(({ firstName, lastName, id, responsibleFor }) => {
+    if (firstName === idOr || id === idOr || lastName === idOr || !idOr) {
+      list[`${firstName} ${lastName}`] = animalsByIds(...responsibleFor).map(({ name }) => name);
+    }
+  });
+  return list;
+}
 
 module.exports = {
   entryCalculator,

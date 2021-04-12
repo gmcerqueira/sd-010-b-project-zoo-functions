8376b253f03e5c51dd5ee485357eeace004de5de
limaluizpaulo <limaluizpaulo@live.com>
2021-04-11 21:37:48 -0300

    Refatorando

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees } = data;
+const { animals } = data;
+const { employees } = data;
+const { prices } = data;
+const { hours } = data;
+
 function animalsByIds(...ids) {
   return animals.filter((animal) => ids.includes(animal.id));
 }
@@ -60,7 +64,6 @@ function animalCount(species) {
 
 function entryCalculator(entrants) {
   if (!entrants) return 0;
-  const prices = Object.entries(data.prices);
   const ent = Object.entries(entrants);
   let entrant = 0;
   ent.forEach((elEnt) => {
@@ -71,102 +74,41 @@ function entryCalculator(entrants) {
   return entrant;
 }
 
-const locations = () => data.animals.map((el) => el.location)
-  .filter((el, index, arr) => arr.indexOf(el) === index);
-
-const isLocalName = (locals) => data.animals.filter((el) => el.location === locals)
-  .map((el) => el.name);
-
-const listNameAnimals = (allLocals) => allLocals.reduce((acumulator, value) => {
-  acumulator[value] = isLocalName(value);
-  return acumulator;
-}, {});
-
-const getNamesAnimalarr = (classAnimal) => classAnimal.map((animal) => animal.name);
-const getNamesAnimalsex = (classAnimal, sex) => classAnimal.filter((animal) => animal.sex === sex)
-  .map((animal) => animal.name);
-
-const isSex = (residents, sex) => (
-  sex !== undefined ? getNamesAnimalsex(residents, sex) : getNamesAnimalarr(residents));
-
-const getNameAnimals = (args) => {
-  const { sex, sorted } = args;
-  const result = data.animals.reduce((acumulador, animal) => {
-    if (!acumulador[animal.location]) acumulador[animal.location] = [];
-    const { name: key, residents } = animal;
-    const objNames = {};
-    const arr = isSex(residents, sex);
-    objNames[key] = sorted ? arr.sort() : arr;
-    acumulador[animal.location].push(objNames);
-    return acumulador;
-  }, {});
-  return result;
-};
-
-function animalMap(options) {
-  if (!options) return listNameAnimals(locations());
-  const { includeNames } = options;
-  if (includeNames) return getNameAnimals(options);
-  return listNameAnimals(locations());
+function animalMap() {
+  // seu código aqui
 }
 
 function schedule(dayName) {
-  const { hours } = data;
-  const result = Object.entries(hours).reduce((acumulador, [key, value]) => {
-    const [am, pm] = Object.values(value);
-    acumulador[key] = key === 'Monday' ? 'CLOSED' : `Open from ${am}am until ${pm % 12}pm`;
-    return acumulador;
-  }, {});
-  if (dayName) {
-    const day = result[dayName];
-    return {
-      [dayName]: day,
-    };
-  }
+  const itens = Object.entries(hours);
+  const result = {};
+  itens.forEach((element) => {
+    if (element[0] === 'Monday') {
+      result[element[0]] = 'CLOSED';
+    } else {
+      result[element[0]] = `Open from ${hours[element[0]]
+        .open}am until ${hours[element[0]].close - 12}pm`;
+    }
+  });
+  if (dayName !== undefined) return { [dayName]: result[dayName] };
   return result;
 }
 
 function oldestFromFirstSpecies(id) {
-  const especieId = data.employees.find(employee => employee.id === id).responsibleFor[0],
-  especie = data.animals.find(animal => animal.id === especieId),
-  olderAnimalAge = Math.max(...especie.residents.map(resident => resident.age));
-  return Object.values(especie.residents.find(resident => resident.age === olderAnimalAge));
+  const especieId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const especie = data.animals.find((animal) => animal.id === especieId);
+  const olderAnimalAge = Math.max(...especie.residents.map((resident) => resident.age));
+  return Object.values(especie.residents.find((resident) => resident.age === olderAnimalAge));
 }
 
-function increasePrices(percentage) {
-  // seu código aqui
+function increasePrices(p) {
+  prices.Adult = Number(((prices.Adult + (Math.ceil(prices.Adult) * (p / 100)))).toFixed(2));
+  prices.Child = Number(Math.round(prices.Child * (1 + (p / 100)) * 100) / 100);
+  prices.Senior = Number(Math.round(prices.Senior * (1 + (p / 100)) * (99 + 1)) / 100);
+  return prices;
 }
 
-
-function employeeCoverage(idOrName) {
-  const getAnimals = (animals, ids, key) => ids.map(id => animals.find(animal => animal[key] === id))
-  .map(lista => lista.name);
-
-const getResponseAnimal = (animal, employees) => {
-  const result = employees.reduce((acumulator, previous) => {
-    const name = `${previous.firstName} ${previous.lastName}`;
-    acumulator[name] = getAnimals(animal, previous.responsibleFor, 'id');
-    return acumulator;
-  }, {});
-  console.log(result);
-  return result;
-};
-
-const getFind = (idOrName, animal, employees) => {
-  const result = employees.reduce((acc, employee) => {
-    const name = `${employee.firstName} ${employee.lastName}`;
-    if (name.includes(idOrName) || employee.id === idOrName) {
-      acc[name] = getAnimals(animal, employee.responsibleFor, 'id');
-    }
-    return acc;
-  }, {});
-  return result;
-};
-
-function employeeCoverage(idOrName) {
-  const { animals, employees } = data;
-  if (!idOrName) return getResponseAnimal(animals, employees);
-  return getFind(idOrName, animals, employees);
+function employeeCoverage() {
+// codigo
 }
 
 module.exports = {

106227aa22ff3e3fa83d75eb44034c0d9a38011b
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-04-12 17:00:11 -0300

    10. IMPLEMENTE A FUNÇÃO schedule

--- src/zoo.js
+++ src/zoo.js
@@ -9,6 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
+const { hours } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -115,21 +116,141 @@ function animalCount(species) {
 
 /* console.log(animalCount('snakes')); */
 
-function entryCalculator(entrants) {
+function entryCalculator(entrants = {}) {
   // seu código aqui
+  let messege = null;
+  if (Object.entries(entrants).length !== 0) {
+    const values = [];
+    prices = data.prices;
+    for (let keyEntrant in entrants) {
+      for (let keyPrice in prices) {
+        if (keyEntrant === keyPrice){
+          let multiplication = entrants[keyEntrant] * prices[keyPrice];
+          values.push(multiplication);
+        }
+      }
+    }
+    messege = values.reduce((a, b) => a + b);
+  } else {
+    messege = 0;
+  }
+  return messege;
 }
 
+const getAnimalsEndLocation = () => (data.animals.map((animal) => (
+  { name: animal.name, location: animal.location })));
+
+const getAnimalsSeparetedByLocale = (location, animalsNameLocation) => {
+  const animalArray = [];
+  const animals = animalsNameLocation.filter((animal) => animal.location === location);
+  animals.forEach((animal) => {
+    animalArray.push(animal.name);
+  });
+  return animalArray;
+};
+
+const getResidentsAnimals = (animalArray) => {
+  animalArray.forEach((animalName) => {
+    const getAnimalOfData = data.animals.find((animal) => animal.name === animalName);
+    console.log(getAnimalOfData.residents);
+  });
+};
+/* const getNamesAnimals = (locations, animalsNameLocation) => {
+  const arrayReturn = {};
+  locations.forEach((location) => {
+    const objAnimals = {};
+    const animalArray = getAnimalsSeparetedByLocale(location, animalsNameLocation);
+    getResidentsAnimals(animalArray);
+  });
+};
+ */
 function animalMap(options) {
   // seu código aqui
+  let messege = null;
+
+  const animalsNameLocation = getAnimalsEndLocation();
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  const arrayReturn = {};
+  if (options === undefined) {
+    locations.forEach((location) => {
+      const objAnimals = {};
+      const animalArray = getAnimalsSeparetedByLocale(location, animalsNameLocation);
+      objAnimals[location] = animalArray;
+      Object.assign(arrayReturn, objAnimals);
+    });
+    messege = arrayReturn;
+  } else if (options.includeNames === true) {
+    // messege = getNamesAnimals(locations, animalsNameLocation);
+  }
+  return messege;
 }
+
+// console.log(animalMap());
+// console.log(animalMap({ includeNames: true }));
 
 function schedule(dayName) {
   // seu código aqui
+  const hoursZoo = data.hours;
+  let messege = null;
+  const scheduleHours = {};
+  Object.keys(hoursZoo).forEach((dayOfWeek) => {
+    scheduleHours[dayOfWeek] = (
+      hoursZoo[dayOfWeek].open === 0
+      && hoursZoo[dayOfWeek].close === 0
+        ? 'CLOSED'
+        : `Open from ${hoursZoo[dayOfWeek].open}am until ${hoursZoo[dayOfWeek].close - 12}pm`);
+  });
+  if (dayName === undefined) {
+    messege = scheduleHours;
+  } else {
+    const hourDayOfWeek = {};
+    hourDayOfWeek[dayName] = scheduleHours[dayName]; messege = hourDayOfWeek;
+  }
+  return messege;
 }
+
+/* console.log(schedule('Monday')); */
+
+const getEmployeeById = (id) => {
+  const employeeSelected = data.employees.find((employee) => (
+    employee.id === id));
+
+  return employeeSelected;
+};
+
+const getOldestAgeOfAnimal = (primerSpecie) => {
+  const animalSpecie = data.animals.find((animal) => (
+    animal.id === primerSpecie
+  ));
+
+  let oldestAgeAnimal = 0;
+  animalSpecie.residents.forEach((animal) => {
+    if (animal.age > oldestAgeAnimal) {
+      oldestAgeAnimal = animal.age;
+    }
+  });
+  return (oldestAgeAnimal);
+};
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeSelected = getEmployeeById(id);
+  const primerSpecie = employeeSelected.responsibleFor[0];
+  const oldestAgeAnimal = getOldestAgeOfAnimal(primerSpecie);
+
+  const especies = data.animals;
+  const getAnimalsByIdSpecie = especies.find((animal) => (animal.id === primerSpecie));
+
+  const getOldestAnimal = getAnimalsByIdSpecie.residents.find((resident) => (
+    resident.age === oldestAgeAnimal
+  ));
+
+  const { name, sex, age } = getOldestAnimal;
+
+  return [name, sex, age];
 }
+
+/* console.log(oldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad')); */
 
 function increasePrices(percentage) {
   // seu código aqui

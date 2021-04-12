38cff839af07fbf2fa301c58401ed8cee345cded
Aladino Borges <eualadino@gmail.com>
2021-04-12 12:59:30 -0300

    Requisito 3: função employeeByName.

--- src/zoo.js
+++ src/zoo.js
@@ -27,6 +27,9 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {

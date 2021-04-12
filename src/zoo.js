17d6853686da307b25ce56fc9df25d1e3198a86f
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-04-12 19:23:19 -0300

    resolucao do requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -45,9 +45,10 @@ function createEmployee(personalInfo, associatedWith) {
   return employ;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((employee) =>
+    employee.managers.find((employeeId) => employeeId === id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -90,7 +91,7 @@ module.exports = {
   employeeByName,
   //   employeeCoverage,
   //   addEmployee,
-  //   isManager,
+  isManager,
   animalsOlderThan,
   //   oldestFromFirstSpecies,
   //   increasePrices,

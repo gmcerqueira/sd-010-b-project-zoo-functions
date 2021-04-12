8f332f099106d5783018e66a03a549315029edf8
Aladino Borges <eualadino@gmail.com>
2021-04-12 13:25:13 -0300

    Requisito 6: função addEmployee.

--- src/zoo.js
+++ src/zoo.js
@@ -45,8 +45,15 @@ function isManager(id) {
   return data.employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {

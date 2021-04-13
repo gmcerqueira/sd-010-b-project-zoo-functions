d1f9e58b462f54f14092ec51f3f74d71a785a55d
João Victor Pistorio <joao.pistorio@gmail.com>
2021-04-13 17:10:50 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -59,6 +59,7 @@ function employeeByName(employeeName) {
   return response;
 }
 
+// Requisito 4
 function createEmployee(personalInfo, associatedWith) {
   let response = []; const obj = {};
 
@@ -71,9 +72,11 @@ function createEmployee(personalInfo, associatedWith) {
   return response;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const response = data.employees.some((employee) => employee.managers.includes(id));
+
+  return response;
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -107,26 +110,6 @@ function createEmployee(personalInfo, associatedWith) {
 //   // seu código aqui
 // }
 
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992',
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-  ],
-};
-
-createEmployee(personalInfo, associatedWith);
-
 module.exports = {
   // entryCalculator,
   // schedule,
@@ -136,7 +119,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,

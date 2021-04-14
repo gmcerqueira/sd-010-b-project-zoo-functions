1942ab20bf601db3647f3f402fd67cebb9eda78b
Nikolas <nikolas.silva1990@gmail.com>
2021-04-14 06:42:08 -0300

    Requisito 1 concluído

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,21 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+// 1. IMPLEMENTE A FUNÇÃO animalsByIds
+// Caso receba nenhum parâmetro, necessário retornar um array vazio
+// Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
+// Ao receber mais de um id, retorna um array com as espécies referentes aos ids
+function animalsByIds(...ids) {
+  if (ids.length <= 0) {
+    return [];
+  }
+  return animals.filter((animal) => ids.includes(animal.id));
 }
+// let filterResult = animalsByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274' , 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
+// console.log(filterResult)
 
 function animalsOlderThan(animal, age) {
   // seu código aqui

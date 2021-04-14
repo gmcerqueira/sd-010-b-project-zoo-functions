2f40c40d4f0e6e12ec0375841f9de1ac9a66424b
Holistic <paulofreire.si@outlook.com>
2021-04-14 12:27:47 -0300

    just testing eslint

--- src/zoo.js
+++ src/zoo.js
@@ -17,50 +17,62 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  console.log(animal, age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  console.log(employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  console.log(personalInfo, associatedWith);
 }
 
 function isManager(id) {
   // seu código aqui
+  console.log();
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  console.log(id, firstName, lastName, managers, responsibleFor);
 }
 
 function animalCount(species) {
   // seu código aqui
+  console.log(species);
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  console.log(entrants);
 }
 
 function animalMap(options) {
   // seu código aqui
+  console.log(options);
 }
 
 function schedule(dayName) {
   // seu código aqui
+  console.log(dayName);
 }
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  console.log(id);
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  console.log(percentage);
 }
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  console.log(idOrName);
 }
 
 module.exports = {

fd6e90c319ca8f56c33176c373131b68fb15c791
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-13 21:14:16 -0300

    tentando a 11

--- src/zoo.js
+++ src/zoo.js
@@ -124,9 +124,20 @@ function schedule(dayName) {
 // console.log(schedule())
 
 function oldestFromFirstSpecies(id) {
-  return id;
+  const filtrarId = data.employees.filter((pessoa) => pessoa.id === id);
+  const primeiroAnimal = filtrarId[0].responsibleFor[0];
+  const filtrarAnimal = data.animals.filter((animal) => animal.id === primeiroAnimal);
+  let idade = 0, nome = '', sexo = '';
+   for (let index = 0; index < filtrarAnimal[0].residents.length; index++) {
+     if (idade < filtrarAnimal[0].residents[index].age) {
+       nome = filtrarAnimal[0].residents[index].name;
+       sexo = filtrarAnimal[0].residents[index].sex;
+       idade = filtrarAnimal[0].residents[index].age;
+     }
+   }
+  return [nome, sexo, idade];
 }
-// console.log(oldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+console.log(oldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 function increasePrices(percentage) {
   return percentage;
 }

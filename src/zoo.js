41f6f282c010e8d4a70b32ce532139f0844eafea
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-15 04:49:57 -0300

    ajuste linter

--- src/zoo.js
+++ src/zoo.js
@@ -78,7 +78,7 @@ function employeeCoverage(idOrName) {
       return true;
     }
     const employeeResponsible = employee.reduce((ac, {firstName, lastName, responsibleFor}) => {
-      const fName = `${ firstName } ${ lastName }`;
+      const fName = ` ${firstName} ${lastName} `;
       ac[fName] = responsibleFor.map((anId) => animals.find((animal) => animal.id === anId).name);
       return ac;
     })

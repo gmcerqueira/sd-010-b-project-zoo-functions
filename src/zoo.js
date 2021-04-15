261900e6a1da56530be0cd60505dc69c11630711
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-15 04:55:16 -0300

    ajuste linter

--- src/zoo.js
+++ src/zoo.js
@@ -77,12 +77,11 @@ function employeeCoverage(idOrName) {
     if (idOrName === em.firstName || idOrName === em.lastName || idOrName === em.id) {
       return true;
     }
-    const employeeResponsible = employee.reduce((ac, {firstName, lastName, responsibleFor}) => {
+    const employeeResponsible = employee.reduce((ac, { firstName, lastName, responsibleFor }) => {
       const fName = ` ${firstName} ${lastName} `;
       ac[fName] = responsibleFor.map((anId) => animals.find((animal) => animal.id === anId).name);
       return ac;
     });
-    {}
     return employeeResponsible;
   });
 }

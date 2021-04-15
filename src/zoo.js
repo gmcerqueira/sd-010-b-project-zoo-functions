9291a01f5fbd2ae0dd8da9645c4dea50b1ad03ea
Guilherme Lima <guilhermelimasky@gmail.com>
2021-04-15 17:08:54 -0300

    requisito 7 fix lint2

--- src/zoo.js
+++ src/zoo.js
@@ -53,8 +53,7 @@ function animalCount(species) {
   const countobj = {};
   if (species === undefined) {
     return countobj;
-  }; 
-  animals.forEach(( {name, residents}) => {
+  } animals.forEach(({ name, residents }) => {
     countobj[name] = residents.length;
   });
   return countobj;

505832f1204a2687a07ace2a4d6b81810a14921b
Guilherme Lima <guilhermelimasky@gmail.com>
2021-04-15 12:27:21 -0300

    Primeiro requisito

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const { animals } = data
+  return animals.filter((animal) => ids.includes(animal.id))
 }
 
 function animalsOlderThan(animal, age) {

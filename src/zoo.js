90d0e396677f1ba29b98ab6030b46e1d72d3a30e
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-16 07:49:04 -0300

    ajuste linter

--- src/zoo.js
+++ src/zoo.js
@@ -83,7 +83,7 @@ function schedule(dayName) {
   });
   if (dayName) return { [dayName]: obj[dayName] };
   return obj;
-}
+};
 console.log(schedule())
 function oldestFromFirstSpecies(id) {
   const checkId = employees.find((employee) => employee.id === id);

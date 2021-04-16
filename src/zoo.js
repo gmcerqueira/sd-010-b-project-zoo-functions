d3b67eb506d085318bc5aab376479a2ed5fda0f1
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-04-16 09:09:47 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -44,9 +44,10 @@ function isManager(id) {
   return employees.some((employee) => employee.managers.includes(id));
 }
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  // seu código aqui
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
+}
 
 // function animalCount(species) {
 //   // seu código aqui
@@ -84,7 +85,7 @@ module.exports = {
   animalsByIds,
   employeeByName,
   //   employeeCoverage,
-  //   addEmployee,
+  addEmployee,
   isManager,
   animalsOlderThan,
   //   oldestFromFirstSpecies,

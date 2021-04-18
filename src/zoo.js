c4787845471317fb8533e99c946ce4ffb5c10bab
Marcos Mantovani <mvmes23@gmail.com>
2021-04-17 22:01:01 -0300

    requisito 1 feito com rest e map

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,31 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) { // vamos colocar o rest como foi mencionado no texto abaixo.
   // seu código aqui
+  // nenhum parametro? retornar um array vazio!
+  // um único parâmetro? retornar? retorna um array com a espécie referente a este id!
+  // ao receber mais de 1 parâmetro? retorna um array com as espécies referente aos Id's passados!
+  const foundAnimals = ids.map((actualId) => {
+    const findAnimal = data.animals.find((animal) =>
+      actualId === animal.id);// retorno implícito. se o id atual do parametro for igual ao id de data.animals.id, entao o find vai retornar o
+      // objeto inteiro que ele achou para dentro do array 'foundAnimal do nosso primeiro map ali em cima'
+    return findAnimal;
+  });
+  // transformar string em objetos.
+  return foundAnimals; // ao ser chamada a função animalsByIds ela irá retornar oque esta alocado na variavel foundAnimals
+  // que nada mais é o array vazio caso nao tenha nenhum parametro "pois o rest sempre retorna um array", ou um array de objetos com os animais
+  // que conseguimos filtrar com nossa função Map + find;
 }
+// /\/\/\/\/\
+// após assistir a aula do isaac chegamos a conclusão de que ao analisar os outros 2 quesitos do problema, teremos que lidar com ele de
+// forma unificada, pois nao sabemos quantos prametros esperar, pode ser 1, 2 ou 1000, portanto existe um operador que usamos que converte
+// X quantidades de parametros em um unico array contendo todos esses parametros separadamente, seu nome é REST, e seu uso se da exclusivamente
+// para declarações de parametros das funções sua sintaxe é literalmente "...parametro", entao ele pega todos parametros passados e joga dentro
+// da variavel parametros em formato de array.
+// resolvido este quesito agora precisamos de uma forma para comparar o id recebido pelo array nos parametros, com o id que está la em data,
+// e se os 2 ids bater, nos retorna um array de objeto, entao basicamente vamos transformar um array em array de objetos, e a melhor função
+// que chegamos a conclusao na aula para este tipo de problema é o MAP.
 
 function animalsOlderThan(animal, age) {
   // seu código aqui

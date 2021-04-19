d6038f3e32ccedc5f88e61e2add9b48c6a1e267c
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-04-19 12:31:05 -0400

    Implementada a funçao createEmployee.

--- src/zoo.js
+++ src/zoo.js
@@ -49,11 +49,12 @@ function employeeByName(employeeName) {
   return verifyEmployee;
 }
 
-/*
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployee = { ...personalInfo, ...associatedWith };
+  return newEmployee;
 }
 
+/*
 function isManager(id) {
   // seu código aqui
 }
@@ -103,10 +104,11 @@ module.exports = {
 
   oldestFromFirstSpecies,
   increasePrices,
-  createEmployee,
+
   */
   animalsByIds,
   animalsOlderThan,
   employeeByName,
+  createEmployee,
 
 };
